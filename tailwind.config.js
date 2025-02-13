97e07caf4cb44e1dcc19266f71cfb5daf7ddfa81
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-13 19:34:41 +0530

    adding colors in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,47 +10,34 @@ module.exports = {
     			sm: 'calc(var(--radius) - 4px)'
     		},
     		colors: {
-    			background: 'hsl(var(--background))',
-    			foreground: 'hsl(var(--foreground))',
-    			card: {
-    				DEFAULT: 'hsl(var(--card))',
-    				foreground: 'hsl(var(--card-foreground))'
-    			},
-    			popover: {
-    				DEFAULT: 'hsl(var(--popover))',
-    				foreground: 'hsl(var(--popover-foreground))'
-    			},
-    			primary: {
-    				DEFAULT: 'hsl(var(--primary))',
-    				foreground: 'hsl(var(--primary-foreground))'
-    			},
-    			secondary: {
-    				DEFAULT: 'hsl(var(--secondary))',
-    				foreground: 'hsl(var(--secondary-foreground))'
-    			},
-    			muted: {
-    				DEFAULT: 'hsl(var(--muted))',
-    				foreground: 'hsl(var(--muted-foreground))'
-    			},
-    			accent: {
-    				DEFAULT: 'hsl(var(--accent))',
-    				foreground: 'hsl(var(--accent-foreground))'
-    			},
-    			destructive: {
-    				DEFAULT: 'hsl(var(--destructive))',
-    				foreground: 'hsl(var(--destructive-foreground))'
-    			},
-    			border: 'hsl(var(--border))',
-    			input: 'hsl(var(--input))',
-    			ring: 'hsl(var(--ring))',
-    			chart: {
-    				'1': 'hsl(var(--chart-1))',
-    				'2': 'hsl(var(--chart-2))',
-    				'3': 'hsl(var(--chart-3))',
-    				'4': 'hsl(var(--chart-4))',
-    				'5': 'hsl(var(--chart-5))'
-    			}
-    		}
+				'mine-shaft': {
+					'50': '#f6f6f6',
+					'100': '#e7e7e7',
+					'200': '#d1d1d1',
+					'300': '#b0b0b0',
+					'400': '#888888',
+					'500': '#6d6d6d',
+					'600': '#5d5d5d',
+					'700': '#4f4f4f',
+					'800': '#454545',
+					'900': '#3d3d3d',
+					'950': '#2d2d2d',
+				},
+				'bright-sun': {
+					'50': '#fffbeb',
+					'100': '#fff3c6',
+					'200': '#ffe588',
+					'300': '#ffd149',
+					'400': '#ffbd20',
+					'500': '#f99b07',
+					'600': '#dd7302',
+					'700': '#b75006',
+					'800': '#943c0c',
+					'900': '#7a330d',
+					'950': '#461902',
+				},
+
+			}
     	}
     },
     plugins: [require("tailwindcss-animate")],
