e00f6180b5d6a1dc29ac552d40b33d03e2045039
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-13 19:06:43 +0530

    adding logo icon

--- src/Header/header.tsx
+++ src/Header/header.tsx
@@ -1,9 +1,13 @@
+import {IconAsset} from "@tabler/icons-react";
 
 const Header = () => {
     return (
         <div className={'w-full text-white flex justify-between items-center p-6 bg-black h-28'}>
-            <div>
-                Logo
+            <div className={'flex gap-3 items-center'}>
+                <IconAsset className={'h-10 w-10'} stroke={1.25}/>
+                <div className={'text-3xl font-bold'}>
+                    iJobs
+                </div>
             </div>
             <div>
                 Links
