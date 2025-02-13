3b81c7997b3eaecaf40839ad04b680f2c70c1c04
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-13 22:13:19 +0530

    adding mantine theme f^n and notification indicator

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-
-
-import { MantineProvider} from '@mantine/core'
+import {createTheme, MantineProvider} from '@mantine/core'
 import './App.css'
 
 import '@mantine/core/styles.css';
@@ -8,11 +6,42 @@ import HomePage from "@/pages/homePage.tsx";
 
 function App() {
 
-  return (
-    <MantineProvider>
-      <HomePage/>
-    </MantineProvider >
-  )
+    const theme = createTheme({
+        colors: {
+            'mine-shaft': [
+                '#f6f6f6',
+                '#e7e7e7',
+                '#d1d1d1',
+                '#b0b0b0',
+                '#888888',
+                '#6d6d6d',
+                '#5d5d5d',
+                '#4f4f4f',
+                '#454545',
+                '#3d3d3d',
+                '#2d2d2d',
+            ],
+            'bright-sun': [
+                '#fffbeb',
+                '#fff3c6',
+                '#ffe588',
+                '#ffd149',
+                '#ffbd20',
+                '#f99b07',
+                '#dd7302',
+                '#b75006',
+                '#943c0c',
+                '#7a330d',
+                '#461902',
+            ],
+        }
+    })
+
+    return (
+        <MantineProvider theme={theme}>
+            <HomePage/>
+        </MantineProvider>
+    )
 }
 
 export default App
--- src/Header/header.tsx
+++ src/Header/header.tsx
@@ -1,10 +1,10 @@
-import { IconBell, IconSettings} from "@tabler/icons-react";
-import {Avatar} from "@mantine/core";
+import {IconBell, IconSettings} from "@tabler/icons-react";
+import {Avatar, Indicator} from "@mantine/core";
 
 const Header = () => {
     return (
-        <div className={'w-full text-white flex justify-between items-center p-6 bg-mine-shaft-950 h-28'}>
-            <div className={'flex gap-3 items-center'}>
+        <div className={'w-full text-white flex justify-between items-center p-6 bg-mine-shaft-950 h-20'}>
+            <div className={'flex gap-3 items-center text-bright-sun-400'}>
                 <img src="src/assets/Fishing-Rod.svg" alt="SVG Icon" className="w-12 h-12"/>
                 <div className={'text-3xl font-bold'}>
                     JobFetch
@@ -16,15 +16,21 @@ const Header = () => {
                 <a href="">Upload Jobs</a>
                 <a href="">About Us</a>
             </div>
-            <div className={'flex gap-5 items-center'}>
-                <IconBell/>
+            <div className={'flex gap-3 items-center'}>
                 <div className={'flex items-center gap-2'}>
                     <div>
                         Marshal
                     </div>
                     <Avatar src={'avatar.png'} alt={"it's me"}/>
                 </div>
-                <IconSettings/>
+                <div className={'bg-mine-shaft-900 p-1.5 rounded-full'}>
+                    <IconSettings stroke={'1.5'}/>
+                </div>
+                <div className={'bg-mine-shaft-900 p-1.5 rounded-full'}>
+                    <Indicator color={'bright-sun.4'} offset={6} size={7} processing>
+                        <IconBell stroke={'1.5'}/>
+                    </Indicator>
+                </div>
             </div>
         </div>
     )
