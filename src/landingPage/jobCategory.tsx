c9f043da40851505e57d287ca93986ba2d3e9a3f
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-14 21:26:32 +0530

    adding jobCategory carousel component

--- src/landingPage/jobCategory.tsx
+++ src/landingPage/jobCategory.tsx
@@ -1,3 +1,7 @@
+import {Carousel, CarouselSlide} from "@mantine/carousel";
+import {jobCategory} from "@/Data/Data.tsx";
+import {IconArrowLeft, IconArrowRight} from "@tabler/icons-react";
+
 const JobCategory = () => {
     return (
         <div className={'mt-20 pb-5'}>
@@ -6,23 +10,36 @@ const JobCategory = () => {
                 Job
             </span> Category
             </div>
-            <div className={'text-lg mx-auto text-mine-shaft-300 text-center w-1/2'}>
+            <div className={'text-lg mb-10 mx-auto text-mine-shaft-300 text-center w-1/2'}>
                 Explore diverse job opportunities tailored to your skills. Start your career journey today!
             </div>
-            <div className={'flex flex-col items-center w-64'}>
-                <div className={'px-2 bg-amber-300 rounded-full'}>
-                    <img className={'h-12 w-8'} src="/src/assets/Category/Digital%20Marketing.png" alt="mar"/>
-                </div>
-                <div className={'text-xl font-semibold text-mine-shaft-100'}>
-                    Digital Marketing
-                </div>
-                <div className={'text-sm text-center text-mine-shaft-300'}>
-                    Explore diverse job opportunities tailored to your skills. Start your career journey today!
-                </div>
-                <div className={'text-xl text-bright-sun-300'}>
-                    1K+ new job posted
-                </div>
-            </div>
+            <Carousel slideSize="18%" slideGap="md" loop className={'focus-visible:[&_button]:!outline-none [&_button]:!bg-bright-sun-400 [&_button]:!border-none [&_button]:hover:opacity-75 [&_button]:opacity-0 hover:[&_button]:opacity-100'}
+                    nextControlIcon={<IconArrowRight className={'h-8 w-8'} />}
+                    previousControlIcon={<IconArrowLeft className={'h-8 w-8'} />}
+                >
+                {jobCategory.map((jc, index) => (
+                    <CarouselSlide key={index}>
+                        <div className={'flex flex-col items-center w-64 gap-2 border border-bright-sun-400 p-5 my-5 rounded-xl hover:cursor-pointer hover:shadow-[0_0_5px_2px_black] transition duration-300 ease-in-out !shadow-bright-sun-300'}>
+                            <div className={'p-2 bg-amber-300 rounded-full'}>
+                                <img
+                                    className={'h-8 w-8'}
+                                    src={`/src/assets/Category/${jc.name}.png`}
+                                    alt={jc.name}/>
+                            </div>
+                            <div className={'text-xl font-semibold text-mine-shaft-100'}>
+                                {jc.name}
+                            </div>
+                            <div className={'text-sm text-center text-mine-shaft-300'}>
+                                {jc.desc}
+                            </div>
+                            <div className={'text-lg text-bright-sun-300'}>
+                                {jc.jobs}+ new job posted
+                            </div>
+                        </div>
+                    </CarouselSlide>
+                ))}
+            </Carousel>
+
         </div>
     )
 }
