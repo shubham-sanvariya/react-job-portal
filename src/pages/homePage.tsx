24baefc2e40b6db47eb6357dd79e84e7c68651c7
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-14 16:49:14 +0530

    adding companies component

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,11 +1,13 @@
 import Header from "@/Header/header.tsx";
 import DreamJob from "@/landingPage/dreamJob.tsx";
+import Companies from "@/landingPage/companies.tsx";
 
 const HomePage = () => {
     return (
         <div className={'min-h-[100vh] bg-mine-shaft-950 font-[poppins]'}>
             <Header/>
             <DreamJob/>
+            <Companies/>
         </div>
     )
 }
