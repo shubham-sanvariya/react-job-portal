5e5c37c301cb595553a720cb313760b49e902072
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-17 17:42:06 +0530

    adding fake testimonials data component

--- src/landingPage/testimonials.tsx
+++ src/landingPage/testimonials.tsx
@@ -1,4 +1,5 @@
 import {Avatar, Rating} from "@mantine/core";
+import {testimonials} from "@/Data/Data.tsx";
 
 const Testimonials = () => {
     return (
@@ -8,18 +9,25 @@ const Testimonials = () => {
                 user
             </span> say's about us ?
             </div>
+            <div className={'flex justify-evenly'}>
 
-            <div className={'flex flex-col gap-3 w-[23%] border rounded-xl mt-10 border-bright-sun-400 p-3'}>
-                <div className={'flex gap-2 items-center'}>
-                    <Avatar className={'!h-14 !w-14'} src="/src/assets/avatar.png" alt={"it's me"}/>
-                    <div>
-                        <div className={'text-lg text-mine-shaft-100 font-semibold'}>Shivam Patel</div>
-                        <Rating value={3.5} fractions={2} readOnly/>
+            {
+                testimonials.map((data, index) => (
+                    <div key={index}
+                         className={'flex flex-col gap-3 w-[23%] border rounded-xl mt-10 border-bright-sun-400 p-3'}>
+                        <div className={'flex gap-2 items-center'}>
+                            <Avatar className={'!h-14 !w-14'} src="/src/assets/avatar.png" alt={"it's me"}/>
+                            <div>
+                                <div className={'text-lg text-mine-shaft-100 font-semibold'}>{data.name}</div>
+                                <Rating value={3.5} fractions={2} readOnly/>
+                            </div>
+                        </div>
+                        <div className={'text-xs text-mine-shaft-300'}>
+                            {data.testimonial}
+                        </div>
                     </div>
-                </div>
-                <div className={'text-xs text-mine-shaft-300'}>
-                    ksjfakjk jfksjffk kjfksjdkfjk kdj  dkfjkd fdfjdkj kjkdjkf
-                </div>
+                ))
+            }
             </div>
         </div>
     )
