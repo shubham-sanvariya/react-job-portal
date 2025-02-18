dbc36966068f99f0c88ec79e06a71416cec22b55
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-18 19:04:37 +0530

    adding findJobs component

---
+++ src/pages/findJobs.tsx
@@ -0,0 +1,9 @@
+
+const FindJobs = () => {
+    return (
+        <div className="min-h-[100vh] bg-mine-shaft-950 font-[poppins]">
+            find jobs
+        </div>
+    )
+}
+export default FindJobs
