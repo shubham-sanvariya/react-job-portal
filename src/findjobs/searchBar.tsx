1a71ddfb88c649a82f5b5f0bd3ff0edba874f659
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-19 17:40:07 +0530

    adding custom slider in searchBar

--- src/findjobs/searchBar.tsx
+++ src/findjobs/searchBar.tsx
@@ -1,8 +1,10 @@
 import MultiInput from "@/findjobs/multiInput.tsx";
 import {dropdownData} from "@/Data/JobsData.tsx";
-import {Divider} from "@mantine/core";
+import {Divider, RangeSlider} from "@mantine/core";
+import {useState} from "react";
 
 const SearchBar = () => {
+    const [value, setValue] = useState<[number, number]>([10, 100]);
     return (
         <div className={'flex px-5 py-8'}>
             {
@@ -15,6 +17,18 @@ const SearchBar = () => {
                     </>
                 )
             }
+            <div className={'w-1/5 [&_.mantine-Slider-label]:!translate-y-10'}>
+                <div className={'flex text-sm justify-between'}>
+                    <div>Salary</div>
+                    <div>&#8377;{value[0]} LPA - &#8377;{value[1]} LPA</div>
+                </div>
+                <RangeSlider size={'xs'} color={'bright-sun.4'} value={value} onChange={setValue} labelTransitionProps={{
+                                 transition: "skew-down",
+                                 duration: 150,
+                                 timingFunction: 'linear'
+                }}
+                />
+            </div>
         </div>
     )
 }
