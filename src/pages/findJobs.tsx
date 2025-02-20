16e4b2bde76c60937a7f10d7ad5c12d05196be07
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-20 17:02:51 +0530

    removing divider

--- src/pages/findJobs.tsx
+++ src/pages/findJobs.tsx
@@ -5,7 +5,6 @@ import Jobs from "@/findjobs/jobs.tsx";
 const FindJobs = () => {
     return (
         <div className="min-h-[100vh] bg-mine-shaft-950 font-[poppins]">
-            <Divider mr={'md'} size={'xs'}/>
             <SearchBar/>
             <Divider mr={'md'} size={'xs'}/>
             <Jobs/>
--- src/pages/findTalent.tsx
+++ src/pages/findTalent.tsx
@@ -5,7 +5,6 @@ import Talents from "@/findTalent/Talents.tsx";
 const FindTalent = () => {
     return (
         <div className="min-h-[90vh] bg-mine-shaft-950 font-[poppins]">
-            <Divider size={'xs'} mx={'md'}/>
             <SearchBar/>
             <Divider size={'xs'} mx={'md'}/>
             <Talents/>
