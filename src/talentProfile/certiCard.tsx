b542c8b43b4ee702cee9766b819804bd7ccae722
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-20 16:36:17 +0530

    adding certiCard component

---
+++ src/talentProfile/certiCard.tsx
@@ -0,0 +1,20 @@
+
+const CertiCard = (props:any) => {
+    return (
+
+        <div className={'flex justify-between'}>
+            <div className={'flex items-center gap-2'}>
+                <div className={'p-2 bg-mine-shaft-800 rounded-md'}><img className={'h-7'} src={`/src/assets/Icons/${props.issuer}.png`} alt="microsoft"/></div>
+                <div className={'flex flex-col'}>
+                    <div className={'font-semibold'}>{props.name}</div>
+                    <div className={'text-sm text-mine-shaft-300'}>{props.company}</div>
+                </div>
+            </div>
+            <div className={'flex flex-col items-end'}>
+                <div className={'text-sm text-mine-shaft-300'}>{props.issueDate}</div>
+                <div className={'text-sm text-mine-shaft-300'}>ID: {props.certificateId}</div>
+            </div>
+        </div>
+    )
+}
+export default CertiCard
