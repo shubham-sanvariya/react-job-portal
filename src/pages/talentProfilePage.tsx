2f3b6fc9368f5d412cee50ca4ebc76be6dc31849
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-20 17:02:26 +0530

    adding recommendTalent component

--- src/pages/talentProfilePage.tsx
+++ src/pages/talentProfilePage.tsx
@@ -1,14 +1,14 @@
-import {Button, Divider} from "@mantine/core";
+import {Button} from "@mantine/core";
 import {Link} from "react-router-dom";
 import {IconArrowLeft} from "@tabler/icons-react";
 import Profile from "@/talentProfile/profile.tsx";
 import {profile} from "@/Data/TalentData.tsx";
+import RecommendTalent from "@/talentProfile/recommendTalent.tsx";
 
 
 const TalentProfilePage = () => {
     return (
         <div className="min-h-[90vh] bg-mine-shaft-950 font-[poppins] p-4">
-            <Divider size={'xs'}/>
             <Link className={'my-4 inline-block'} to={'/find-talent'}>
                 <Button leftSection={<IconArrowLeft size={20}/>} color={'bright-sun.4'} variant={"light"}>
                     Back
@@ -16,6 +16,7 @@ const TalentProfilePage = () => {
             </Link>
             <div className={'flex gap-5'}>
                 <Profile {...profile}/>
+                <RecommendTalent/>
             </div>
         </div>
     )
