c013959df496e90450353c3ab0c2dbb7333ead1d
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-21 20:32:57 +0530

    adding RecommendedJobs component

---
+++ src/jobDesc/RecommendedJobs.tsx
@@ -0,0 +1,21 @@
+
+import JobCard from "@/findjobs/jobCard.tsx";
+import {jobList} from "@/Data/JobsData.tsx";
+
+const RecommendedJobs = () => {
+    return (
+        <div>
+            <div className={'text-xl font-semibold mb-5'}>
+                Recommended Jobs
+            </div>
+            <div className={'flex flex-col flex-wrap gap-5 '}>
+                {
+                    jobList.map((job,index) => index < 4 && (
+                        <JobCard key={index} {...job}/>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+export default RecommendedJobs
