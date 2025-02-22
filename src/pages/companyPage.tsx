109d20b6d1efdb7d091fab66027f050c54178bc3
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-22 16:26:07 +0530

    adding similarCompanies component

--- src/pages/companyPage.tsx
+++ src/pages/companyPage.tsx
@@ -2,17 +2,20 @@ import {useNavigate} from "react-router-dom";
 import {Button} from "@mantine/core";
 import {IconArrowLeft} from "@tabler/icons-react";
 import Company from "@/companyProfile/company.tsx";
+import SimilarCompanies from "@/companyProfile/similarCompanies.tsx";
 
 
 const CompanyPage = () => {
     const navigate = useNavigate();
     return (
         <div className="min-h-[90vh] bg-mine-shaft-950 font-[poppins] p-4">
-            <Button onClick={() => navigate(-1)}  my={"md"} leftSection={<IconArrowLeft size={20}/>} color={'bright-sun.4'} variant={"light"}>
+            <Button onClick={() => navigate(-1)} my={"md"} leftSection={<IconArrowLeft size={20}/>}
+                    color={'bright-sun.4'} variant={"light"}>
                 Back
             </Button>
-            <div className={'flex gap-5'}>
-<Company/>
+            <div className={'flex justify-between gap-5'}>
+                <Company/>
+                <SimilarCompanies/>
             </div>
         </div>
     )
