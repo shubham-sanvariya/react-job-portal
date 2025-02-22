9c2059109056ca65101c3e7cc31ad5e773fabbd3
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-22 22:59:51 +0530

    adding postedJobDesc component

--- src/findTalent/talentCard.tsx
+++ src/findTalent/talentCard.tsx
@@ -1,8 +1,15 @@
-import { IconHeart, IconMapPin} from "@tabler/icons-react";
-import {Avatar, Button, Divider, Text} from "@mantine/core";
+import {IconCalendarMonth, IconHeart, IconMapPin} from "@tabler/icons-react";
+import {Avatar, Button, Divider, Modal, Text} from "@mantine/core";
 import {Link} from "react-router-dom";
+import {useDisclosure} from "@mantine/hooks";
+import {DateInput, TimeInput} from "@mantine/dates";
+import {useRef, useState} from "react";
 
 const TalentCard = (props: any) => {
+    const [value, setValue] = useState<Date | null>(null);
+    const [opened, {open, close}] = useDisclosure(false);
+    const ref = useRef<HTMLInputElement>(null);
+
     return (
         <div
             className={'flex flex-col gap-3 rounded-xl bg-mine-shaft-900 p-4 w-96 hover:shadow-[0_0_5px_1px_yellow] !shadow-bright-sun-400'}>
@@ -23,9 +30,9 @@ const TalentCard = (props: any) => {
             <div
                 className={'flex gap-2 '}>
                 {
-                    props.topSkills.map((skill : string[] ,index : number) => (
+                    props.topSkills.map((skill: string[], index: number) => (
                         <div key={index}
-                            className={'p-2 py-1 bg-mine-shaft-800 text-bright-sun-400 rounded-lg text-xs'}>{skill}</div>
+                             className={'p-2 py-1 bg-mine-shaft-800 text-bright-sun-400 rounded-lg text-xs'}>{skill}</div>
                     ))
                 }
             </div>
@@ -33,24 +40,67 @@ const TalentCard = (props: any) => {
                 {props.about}
             </Text>
             <Divider color={'mine-shaft.7'} size={'xs'}/>
-            <div className={'flex justify-between'}>
-                <div className={'font-semibold text-mine-shaft-200'}>
-                    {props.expectedCtc}
-                </div>
-                <div className={'flex gap-1 text-xs text-mine-shaft-400 items-center'}>
-                    <IconMapPin className={'h-5 w-5'} stroke={1.5}/>
-                    {props.location}
-                </div>
-            </div>
+            {
+                props.invited ? <div className={'flex items-center text-mine-shaft-200 text-sm gap-1'}>
+                        <IconCalendarMonth className={'w-5 h-5'}/>Interview: August 27, 2024 10:00 AM
+                    </div>
+                    :
+                    <div className={'flex justify-between'}>
+                        <div className={'font-semibold text-mine-shaft-200'}>
+                            {props.expectedCtc}
+                        </div>
+                        <div className={'flex gap-1 text-xs text-mine-shaft-400 items-center'}>
+                            <IconMapPin className={'h-5 w-5'} stroke={1.5}/>
+                            {props.location}
+                        </div>
+                    </div>
+            }
             <Divider color={'mine-shaft.7'} size={'xs'}/>
             <div className={'flex [&>*]:w-1/2 [&>*]:p-1'}>
+                {!props.invited && <>
                 <Link to={'/talent-profile'}>
                     <Button color={'bright-sun.4'} variant={"outline"} fullWidth>Profile</Button>
                 </Link>
-                <div>
-                    <Button color={'bright-sun.4'} variant={"light"}  fullWidth>Message</Button>
-                </div>
+                    <div>
+                        {props.posted ?
+                            <Button onClick={open} rightSection={<IconCalendarMonth className={'w-5 h-5'}/>}
+                                    color={'bright-sun.4'} variant={"light"} fullWidth>Schedule
+                            </Button>
+                            : <Button color={'bright-sun.4'} variant={"light"} fullWidth>Message</Button>}
+                    </div>
+                </>
+                }
+                {
+                    props.invited &&
+                    <>
+                        <div>
+                            <Button color={'bright-sun.4'} variant={"outline"} fullWidth>Accept</Button>
+                        </div>
+                        <div>
+                            <Button color={'red.7'} variant={"light"} fullWidth>Reject</Button>
+                        </div>
+                    </>
+                }
             </div>
+            <Modal opened={opened} onClose={close} title={'Schedule Interview'} centered>
+                <div className={'flex flex-col gap-4'}>
+                    <DateInput
+                        minDate={new Date()}
+                        value={value}
+                        onChange={setValue}
+                        label={'Date'}
+                        placeholder={'Enter Date'}
+                    />
+                    <TimeInput
+                        minTime={'09:00'}
+                        maxTime={'17:00'}
+                        ref={ref}
+                        onClick={() => ref.current?.showPicker()}
+                        label={'Time'}
+                    />
+                    <Button color={'bright-sun.4'} variant={"light"} fullWidth>Schedule</Button>
+                </div>
+            </Modal>
         </div>
     )
 }
