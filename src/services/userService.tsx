9469b1ed27f48a150fced8989f9f863410db822f
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-02-28 13:48:46 +0530

    adding sendOtp, verifyOtp, changePassword method in usersrevice

--- src/services/userService.tsx
+++ src/services/userService.tsx
@@ -6,7 +6,7 @@ const registerUser = async ( user: any ) => {
     try {
         const response = await axios.post(`${base_URL}/register`, user);
         return response.data;
-    } catch (error : any) {
+    } catch (error : unknown) {
         console.log(error);
         throw error;
     }
@@ -16,10 +16,40 @@ const loginUser = async ( login: any ) => {
     try {
         const response = await axios.post(`${base_URL}/login`, login);
         return response.data;
-    } catch (error : any) {
+    } catch (error : unknown) {
         console.log(error);
         throw error;
     }
 }
 
-export {registerUser, loginUser};
+const sendOtp = async ( email : string) => {
+    try {
+        const res = await axios.post(`${base_URL}/sendOtp/${email}`);
+        return res.data;
+    }catch (error : unknown) {
+        console.log(error);
+        throw error;
+    }
+}
+
+const verifyOtp = async ( email : string, otp : string) => {
+    try {
+        const res = await axios.get(`${base_URL}/verifyOtp/${email}/${otp}`);
+        return res.data;
+    }catch (error : unknown) {
+        console.log(error);
+        throw error;
+    }
+}
+
+const changePassword = async ( email : string, password : string) => {
+    try {
+        const res = await axios.post(`${base_URL}/changePass`, {email,password});
+        return res.data;
+    }catch (error : unknown) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export {registerUser, loginUser, sendOtp, verifyOtp, changePassword};
