0e1bdf4c30e54a8bcdc219e0887d3ca39666918e
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-01 22:29:20 +0530

    adding formatDate in utilService

---
+++ src/services/utilService.tsx
@@ -0,0 +1,10 @@
+
+const formatDate = ( dateString : string) => {
+    const date = new Date(dateString);
+    const options = { year : 'numeric' as const, month : 'short' as const};
+    return date.toLocaleString('en-US',options)
+}
+
+export {
+    formatDate
+};
