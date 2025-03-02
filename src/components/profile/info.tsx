28daeb6c265da006918ae94a7712ea49028a39b7
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-02 16:12:24 +0530

    adding info component

---
+++ src/components/profile/info.tsx
@@ -0,0 +1,77 @@
+import {ActionIcon} from "@mantine/core";
+import {IconBriefcase, IconDeviceFloppy, IconMapPin, IconPencil} from "@tabler/icons-react";
+import SelectInput from "@/components/profile/selectInput.tsx";
+import fields from "@/Data/Profile.tsx";
+import {useDispatch, useSelector} from "react-redux";
+import {selectProfile, updateProfileAsyncThunk} from "@/slices/profileSlice.tsx";
+import {useState} from "react";
+import {hasLength, useForm} from "@mantine/form";
+import {selectUser} from "@/slices/userSlice.tsx";
+import {ProfileType} from "@/types/profileType.ts";
+import {successNotification} from "@/services/notificationServices.tsx";
+import {AppDispatch} from "@/store.tsx";
+
+const Info = () => {
+    const dispatch = useDispatch<AppDispatch>();
+    const profileState = useSelector(selectProfile);
+    const userState = useSelector(selectUser);
+    const [edit, setEdit] = useState<boolean>(false);
+
+    const handleEdit = () => {
+        if (!edit) {
+            setEdit(true)
+            form.setValues({
+                jobTitle: profileState?.jobTitle,
+                company: profileState?.company,
+                location: profileState?.location
+            })
+        }
+        else {
+            setEdit(false);
+            const updatedProfile = {...profileState, ...form.getValues()};
+            dispatch(updateProfileAsyncThunk(updatedProfile as ProfileType));
+            successNotification("Success","Profile Updated Successfully.")
+        }
+    }
+
+    const form = useForm({
+        mode: "controlled",
+        initialValues: { jobTitle : "", company: "", location : ""},
+        validate : {
+            jobTitle : hasLength({ min : 3 }, 'Must be at least 3 Characters')
+            ,company : hasLength({ min : 3 }, 'Must be at least 3 Characters')
+            ,location : hasLength({ min : 3 }, 'Must be at least 3 Characters'),
+        }
+    })
+
+    return (
+        <>
+            <div className={'flex justify-between text-3xl font-semibold '}>{userState.name}
+                <ActionIcon size={"lg"} color={'bright-sun.4'} variant={'subtle'}
+                            onClick={handleEdit}
+                >
+                    {edit ? <IconDeviceFloppy className={'h-4/5 w-4/5'}/> :
+                        <IconPencil className={'h-4/5 w-4/5'}/>}
+                </ActionIcon>
+            </div>
+            {edit ? <>
+                    <div className={'flex gap-10 [&>*]:w-1/2'}>
+                        <SelectInput form={form} name={"jobTitle"} {...fields[0]} />
+                        <SelectInput form={form} name={"company"} {...fields[1]} />
+                    </div>
+                    <SelectInput form={form} name={"location"} {...fields[2]} />
+                </>
+                :
+                <>
+                    <div className={'text-xl flex gap-1 items-center'}><IconBriefcase
+                        className={'h-5 w-5'}/> {profileState?.jobTitle} &bull; {profileState?.company}
+                    </div>
+                    <div className={'flex gap-1 text-xs text-mine-shaft-400 items-center'}>
+                        <IconMapPin className={'h-5 w-5'} stroke={1.5}/> {profileState?.location}
+                    </div>
+                </>
+            }
+        </>
+    )
+}
+export default Info
--- src/components/profile/profile.tsx
+++ src/components/profile/profile.tsx
@@ -1,27 +1,45 @@
-import {IconBriefcase, IconDeviceFloppy, IconMapPin, IconPencil, IconPlus} from "@tabler/icons-react";
+import {IconDeviceFloppy, IconPencil, IconPlus} from "@tabler/icons-react";
 import {ActionIcon, Divider, TagsInput, Textarea} from "@mantine/core";
 import CertiCard from "@/components/profile/certiCard.tsx";
 import ExpCard from "@/components/profile/expCard.tsx";
-import {profile} from "@/Data/TalentData.tsx";
-import {useState} from "react";
-import SelectInput from "@/components/profile/selectInput.tsx";
-import fields from "@/Data/Profile.tsx";
+import {useEffect, useState} from "react";
 import ExpInput from "@/components/profile/expInput.tsx";
 import CertiInput from "@/components/profile/certiInput.tsx";
+import {getProfileAsyncThunk, selectProfile} from "@/slices/profileSlice.tsx";
+import {useDispatch, useSelector} from "react-redux";
+import {selectUser} from "@/slices/userSlice.tsx";
+import {Certification, Experience} from "@/types/profileType.ts";
+import Info from "@/components/profile/info.tsx";
+import {AppDispatch} from "@/store.tsx";
 
 
 const Profile = () => {
+    const dispatch = useDispatch<AppDispatch>();
+    const userState = useSelector(selectUser);
+    const profileState = useSelector(selectProfile);
+    const [about, setAbout] = useState("");
+    const [skills, setSkills] = useState([]);
     const [edit, setEdit] = useState<boolean[]>([false, false, false, false, false]);
     const [addExp, setAddExp] = useState(false);
     const [addCerti, setAddCerti] = useState(false);
-    const [about, setAbout] = useState(profile.about);
-    const [skills, setSkills] = useState(profile.skills);
+    // const [profileState, setProfileState] = useState<ProfileType | null>(ps);
 
     const handleEdit = (index: number) => {
         const editNew = [...edit];
         editNew[index] = !editNew[index];
         setEdit(editNew);
     }
+
+    useEffect(() => {
+        if (userState.profileId) {
+            dispatch(getProfileAsyncThunk(Number(userState.profileId)));
+        }
+    }, []);
+
+    useEffect(()=>{
+        console.log(profileState)
+    },[profileState])
+
     return (
         <div className={'w-4/5 mx-auto'}>
             <div className={'relative'}>
@@ -31,31 +49,7 @@ const Profile = () => {
                     src={`src/assets/avatar.png`} alt={""}/>
             </div>
             <div className={'px-3 mt-16'}>
-                <div className={'flex justify-between text-3xl font-semibold '}>{profile.name}
-                    <ActionIcon size={"lg"} color={'bright-sun.4'} variant={'subtle'}
-                                onClick={() => handleEdit(0)}
-                    >
-                        {edit[0] ? <IconDeviceFloppy className={'h-4/5 w-4/5'}/> :
-                            <IconPencil className={'h-4/5 w-4/5'}/>}
-                    </ActionIcon>
-                </div>
-                {edit[0] ? <>
-                        <div className={'flex gap-10 [&>*]:w-1/2'}>
-                            <SelectInput {...fields[0]} />
-                            <SelectInput {...fields[1]} />
-                        </div>
-                        <SelectInput {...fields[2]} />
-                    </>
-                    :
-                    <>
-                        <div className={'text-xl flex gap-1 items-center'}><IconBriefcase
-                            className={'h-5 w-5'}/> {profile.role} &bull; {profile.company}
-                        </div>
-                        <div className={'flex gap-1 text-xs text-mine-shaft-400 items-center'}>
-                            <IconMapPin className={'h-5 w-5'} stroke={1.5}/> {profile.location}
-                        </div>
-                    </>
-                }
+                <Info/>
             </div>
             <Divider mx={'xs'} my={"xl"}/>
             <div className={'px-3'}>
@@ -71,12 +65,12 @@ const Profile = () => {
                         placeholder={'Enter About YourSelf...'}
                         minRows={3}
                         autosize
-                        value={about}
+                        value={profileState?.about}
                         onChange={(event) => setAbout(event.currentTarget.value)}
                     />
                     :
                     <div className={'text-sm text-mine-shaft-300 text-justify'}>
-                        {about}
+                        {profileState?.about}
                     </div>
                 }
             </div>
@@ -93,14 +87,14 @@ const Profile = () => {
                 {
                     edit[2] ? <TagsInput
                             value={skills}
-                            onChange={setSkills}
+                            // onChange={setSkills}
                             placeholder={'Enter tag'}
                             splitChars={[',', ' ', '|']}
                         />
                         :
                         <div className={'flex flex-wrap gap-2'}>
                             {
-                                skills.map((skill: string, index: number) => (
+                                profileState?.skills?.map((skill: string, index: number) => (
                                     <div key={index}
                                          className={'bg-bright-sun-300 font-medium fow bg-opacity-15 rounded-3xl text-bright-sun-400 px-3 py-1'}>
                                         {skill}
@@ -131,7 +125,7 @@ const Profile = () => {
                 </div>
                 <div className={'flex flex-col gap-8'}>
                     {
-                        profile.experience.map((exp: object, index: number) => (
+                        profileState?.experiences?.map((exp: Experience, index: number) => (
                             <ExpCard key={index} {...exp} edit={edit[3]}/>
                         ))
                     }
@@ -159,7 +153,7 @@ const Profile = () => {
                 </div>
                 <div className={'flex flex-col gap-8'}>
                     {
-                        profile.certifications.map((certi: object, index: number) => (
+                        profileState?.certificates?.map((certi: Certification, index: number) => (
                             <CertiCard key={index} edit={edit[4]} {...certi}/>
                         ))
                     }
