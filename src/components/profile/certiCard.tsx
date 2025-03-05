5803a4ae1831ebcc165816ec24f814d685b80e6b
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-05 12:58:15 +0530

    adding add & delete certification functionality

--- src/components/profile/certiCard.tsx
+++ src/components/profile/certiCard.tsx
@@ -1,8 +1,26 @@
 import {IconTrash} from "@tabler/icons-react";
 import {ActionIcon} from "@mantine/core";
 import {formatDate} from "@/services/utilService.tsx";
+import {useDispatch, useSelector} from "react-redux";
+import {AppDispatch} from "@/store.tsx";
+import {selectProfile, updateProfileAsyncThunk} from "@/slices/profileSlice.tsx";
+import {CertificationType, ProfileType} from "@/types/profileType.ts";
+import {successNotification} from "@/services/notificationServices.tsx";
 
 const CertiCard = (props: any) => {
+
+    const dispatch = useDispatch<AppDispatch>();
+    const profileState = useSelector(selectProfile);
+
+    const handleDelete = () => {
+        const certis : CertificationType[] | [] = [...(profileState?.certificates || [] )];
+
+        certis.splice(props.index, 1);
+        const updatedProfile = {...profileState, certificates : certis};
+        dispatch(updateProfileAsyncThunk(updatedProfile as ProfileType));
+        successNotification("Success", `Certificate Deleted Successfully`);
+    }
+
     return (
 
         <div className={'flex justify-between'}>
@@ -22,7 +40,7 @@ const CertiCard = (props: any) => {
                     <div className={'text-sm text-mine-shaft-300'}>ID: {props.certificateId}</div>
                 </div>
 
-                {props.edit && <ActionIcon color={'red.8'} variant={'subtle'}>
+                {props.edit && <ActionIcon onClick={handleDelete} color={'red.8'} variant={'subtle'}>
                     <IconTrash
                         className="h-4/5 w-4/5"
                         stroke={1.5}
