d6634b05f3be4dda17ea4fac305878df11e40883
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-06 18:10:21 +0530

    adding onUpdate on description field

--- src/components/postJob/textEditor.tsx
+++ src/components/postJob/textEditor.tsx
@@ -6,10 +6,9 @@ import Underline from '@tiptap/extension-underline';
 import TextAlign from '@tiptap/extension-text-align';
 import Superscript from '@tiptap/extension-superscript';
 import SubScript from '@tiptap/extension-subscript';
-import {content} from "@/Data/PostJob.tsx";
 
 
-const TextEditor = () => {
+const TextEditor = ( props : any ) => {
     const editor = useEditor({
         extensions: [
             StarterKit,
@@ -20,7 +19,10 @@ const TextEditor = () => {
             Highlight,
             TextAlign.configure({ types: ['heading', 'paragraph'] }),
         ],
-        content,
+        content : props.form.getValues().description,
+        onUpdate({ editor }) {
+            props.form.setFieldValue('description', editor.getHTML())
+        }
     });
 
     return (
