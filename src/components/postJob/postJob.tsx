14d613556f9c3952dac2035d381ee130408ca8f6
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-06 18:17:48 +0530

    adding form & it's validation

--- src/components/postJob/postJob.tsx
+++ src/components/postJob/postJob.tsx
@@ -1,33 +1,74 @@
 import SelectInput from "@/components/postJob/selectInput.tsx";
-import {fields} from "@/Data/PostJob.tsx";
-import {Button, TagsInput} from "@mantine/core";
+import {content, fields} from "@/Data/PostJob.tsx";
+import {Button, NumberInput, TagsInput, Textarea} from "@mantine/core";
 import TextEditor from "@/components/postJob/textEditor.tsx";
+import {hasLength, isNotEmpty, useForm} from "@mantine/form";
 
 const PostJob = () => {
+    const form = useForm({
+        mode: "controlled",
+        validateInputOnChange: true,
+        initialValues: {
+            jobTitle : "",
+            company : "",
+            // applicants : [],
+            experience : "",
+            jobType : "",
+            location : "",
+            packageOffered : "",
+            skillRequired : [],
+            about : "",
+            description : content,
+            // postTime : "",
+            // jobStatus : "",
+        },
+        validate:{
+            jobTitle: hasLength({min: 4}, 'Must be at least 4 Characters')
+            , company: hasLength({min: 4}, 'Must be at least 4 Characters'),
+            experience: isNotEmpty('Experience is required.'),
+            jobType: isNotEmpty("Job type is required.")
+            , location: hasLength({min: 4}, 'Must be at least 3 Characters'),
+            packageOffered: isNotEmpty("Package is required"),
+            skillRequired: isNotEmpty("Skills are required"),
+            about: hasLength({min: 30}, "Must be at least 30 Characters")
+            , description: hasLength({min: 50}, 'Must be at least 50 Characters'),
+        }
+    })
     return (
         <div className={'w-4/5 mx-auto'}>
             <div className={'text-2xl font-semibold mb-5'}>Post a Job</div>
             <div className={'flex flex-col gap-5'}>
                 <div className={'flex gap-10 [&>*]:w-1/2'}>
-                    <SelectInput {...fields[0]} />
-                    <SelectInput {...fields[1]} />
+                    <SelectInput form={form} name={"jobTitle"} {...fields[0]} />
+                    <SelectInput form={form} name={"company"} {...fields[1]} />
                 </div>
                 <div className={'flex gap-10 [&>*]:w-1/2'}>
-                    <SelectInput {...fields[2]} />
-                    <SelectInput {...fields[3]} />
+                    <SelectInput form={form} name={"experience"} {...fields[2]} />
+                    <SelectInput form={form} name={"jobType"} {...fields[3]} />
                 </div>
                 <div className={'flex gap-10 [&>*]:w-1/2'}>
-                    <SelectInput {...fields[4]} />
-                    <SelectInput {...fields[5]} />
+                    <SelectInput form={form} name={"location"} {...fields[4]} />
+                    <NumberInput {...form.getInputProps('packageOffered')} label={"Salary"} min={1} max={300}
+                            placeholder={"Enter Salary"}  clampBehavior={"strict"}   hideControls />
                 </div>
-                <TagsInput withAsterisk label={"skills"} placeholder={'Enter Skills'} clearable acceptValueOnBlur
+                <TagsInput {...form.getInputProps('skillRequired')}  withAsterisk label={"skills"} placeholder={'Enter Skills'} clearable acceptValueOnBlur
                            splitChars={[',', ' ', '|']}
+                />
+                <Textarea
+                    {...form.getInputProps('about')}
+                    withAsterisk
+                    className="my-3"
+                    label={"About Job"}
+                    placeholder={"Enter about job..."}
+                    minRows={2}
+                    autosize
                 />
                 <div className={'[&_button[data-active="true"]]:!text-bright-sun-400 [&_button[data-active="true"]]:bg-bright-sun-400/20'}>
                     <div className="text-sm font-medium">
                         Job Description
+                        <span className="text-red-500">*</span>
                     </div>
-                    <TextEditor/>
+                    <TextEditor form={form}/>
                 </div>
                 <div className={'flex gap-4'}>
                     <Button color={'bright-sun.4'} variant={"light"}>
--- src/components/postJob/selectInput.tsx
+++ src/components/postJob/selectInput.tsx
@@ -4,6 +4,8 @@ import {Combobox, InputBase, ScrollArea, useCombobox} from '@mantine/core';
 const SelectInput = (props: any) => {
     useEffect(() => {
         setData(props.options)
+        setValue(props.form.getInputProps(props.name).value);
+        setSearch(props.form.getInputProps(props.name).value);
     }, []);
     const combobox = useCombobox({
         onDropdownClose: () => combobox.resetSelectedOption(),
@@ -16,7 +18,7 @@ const SelectInput = (props: any) => {
     const exactOptionMatch = data.some((item) => item === search);
     const filteredOptions = exactOptionMatch
         ? data
-        : data.filter((item) => item.toLowerCase().includes(search.toLowerCase().trim()));
+        : data.filter((item) => item.toLowerCase().includes(search?.toLowerCase().trim()));
 
     const options = filteredOptions.map((item) => (
         <Combobox.Option value={item} key={item}>
@@ -32,9 +34,11 @@ const SelectInput = (props: any) => {
                 if (val === '$create') {
                     setData((current) => [...current, search]);
                     setValue(search);
+                    props.form.setFieldValue(props.name,search);
                 } else {
                     setValue(val);
                     setSearch(val);
+                    props.form.setFieldValue(props.name,val);
                 }
 
                 combobox.closeDropdown();
@@ -42,7 +46,7 @@ const SelectInput = (props: any) => {
         >
             <Combobox.Target>
                 <InputBase
-                    className="[&_input]:font-medium"
+                    {...props.form.getInputProps(props.name)}
                     withAsterisk
                     label={props.label}
                     rightSection={<Combobox.Chevron/>}
@@ -67,7 +71,7 @@ const SelectInput = (props: any) => {
                 <Combobox.Options>
                     <ScrollArea.Autosize mah={200} type={"scroll"}>
                         {options}
-                        {!exactOptionMatch && search.trim().length > 0 && (
+                        {!exactOptionMatch && search?.trim().length > 0 && (
                             <Combobox.Option value="$create">+ Create {search}</Combobox.Option>
                         )}
                     </ScrollArea.Autosize>
