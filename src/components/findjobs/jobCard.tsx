02fa1ec64bc288603b9815ca91ba2cd14bc7033f
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-07 13:13:07 +0530

    using jobsState

--- src/components/findjobs/jobCard.tsx
+++ src/components/findjobs/jobCard.tsx
@@ -1,9 +1,9 @@
 import {IconBookmark, IconClockHour3} from "@tabler/icons-react";
 import {Divider, Text} from "@mantine/core";
-import {jobListProps} from "@/Data/JobsData.tsx";
 import {Link} from "react-router-dom";
+import {JobType} from "@/types/jobType.ts";
 
-const JobCard = (props : jobListProps) => {
+const JobCard = (props : JobType) => {
     return (
         <Link to={'/jobs'} className={'flex flex-col gap-3 rounded-xl bg-mine-shaft-900 p-4 w-72 hover:shadow-[0_0_5px_1px_yellow] !shadow-bright-sun-400'}>
             <div className={'flex justify-between'}>
@@ -11,7 +11,7 @@ const JobCard = (props : jobListProps) => {
                     <div className={'p-2 bg-mine-shaft-800 rounded-md'}><img className={'h-7'} src={`/src/assets/Icons/${props.company}.png`} alt="microsoft"/></div>
                     <div>
                         <div className={'font-semibold'}>{props.jobTitle}</div>
-                        <div className={'text-xs text-mine-shaft-300'}>{props.company} &#x2022; {props.applicants} Applicants</div>
+                        <div className={'text-xs text-mine-shaft-300'}>{props.company} &#x2022; {props.applicants ? props.applicants.length : 0} Applicants</div>
                     </div>
                 </div>
                 <IconBookmark className={'text-mine-shaft-300 cursor-pointer'}/>
@@ -23,16 +23,16 @@ const JobCard = (props : jobListProps) => {
                 <div>{props.location}</div>
             </div>
             <Text className="!text-xs text-justify !text-mine-shaft-300" lineClamp={3}>
-                {props.description}
+                {props.about}
             </Text>
             <Divider color={'mine-shaft.7'} size={'xs'}/>
             <div className={'flex justify-between'}>
                 <div className={'font-semibold text-mine-shaft-200'}>
-                    &#8377; {props.package}
+                    &#8377; {props.packageOffered}
                 </div>
                 <div className={'flex gap-1 text-xs text-mine-shaft-400 items-center'}>
                     <IconClockHour3 className={'h-5 w-5'} stroke={1.5}/>
-                    {props.postedDaysAgo}
+                    {props.postTime}
                 </div>
             </div>
         </Link>
--- src/components/findjobs/jobs.tsx
+++ src/components/findjobs/jobs.tsx
@@ -1,8 +1,19 @@
 import Sort from "@/components/findjobs/sort.tsx";
 import JobCard from "@/components/findjobs/jobCard.tsx";
-import {jobList} from "@/Data/JobsData.tsx";
+import {useDispatch, useSelector} from "react-redux";
+import {AppDispatch} from "@/store.tsx";
+import {getJobsAsyncThunk, selectJobs} from "@/slices/jobSlice.ts";
+import {useEffect} from "react";
 
 const Jobs = () => {
+    const dispatch = useDispatch<AppDispatch>();
+    const jobsState = useSelector(selectJobs);
+
+    useEffect(() => {
+        dispatch(getJobsAsyncThunk());
+        console.log(jobsState)
+
+    }, []);
     return (
         <div className={'p-5'}>
             <div className={'flex justify-between'}>
@@ -11,7 +22,7 @@ const Jobs = () => {
             </div>
             <div className={'flex flex-wrap mt-10 gap-5'}>
                 {
-                    jobList.map((job, index) => (
+                    jobsState?.map((job, index) => (
                         <JobCard key={index} {...job}/>
                     ))
                 }
