4d9e0052113745dfb9bb205fbf90acde71df6f51
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-08 19:17:19 +0530

    adding save jobs method in job card component

--- src/components/findjobs/jobCard.tsx
+++ src/components/findjobs/jobCard.tsx
@@ -1,21 +1,41 @@
-import {IconBookmark, IconClockHour3} from "@tabler/icons-react";
+import {IconBookmark, IconBookmarkFilled, IconClockHour3} from "@tabler/icons-react";
 import {Divider, Text} from "@mantine/core";
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import {JobType} from "@/types/jobType.ts";
 import {timeAgo} from "@/services/utilService.tsx";
+import useJob from "@/hooks/useJob.tsx";
+import {useSelector} from "react-redux";
+import {selectProfile} from "@/slices/profileSlice.tsx";
 
 const JobCard = (props : JobType) => {
+    const navigate = useNavigate();
+    const profileState = useSelector(selectProfile);
+    const { handleSaveJobs } = useJob();
+
     return (
         <Link to={`/jobs/${props.id}`} className={'flex flex-col gap-3 rounded-xl bg-mine-shaft-900 p-4 w-72 hover:shadow-[0_0_5px_1px_yellow] !shadow-bright-sun-400'}>
             <div className={'flex justify-between'}>
                 <div className={'flex items-center gap-2'}>
-                    <div className={'p-2 bg-mine-shaft-800 rounded-md'}><img className={'h-7'} src={`/src/assets/Icons/${props.company}.png`} alt="microsoft"/></div>
+                    <div className={'p-2 bg-mine-shaft-800 rounded-md'} onClick={(e) => {
+                        e.preventDefault();
+                        e.stopPropagation(); // Prevent parent <Link> navigation
+                        navigate(`/company/${props.company}`);
+                    }}><img className={'h-7'} src={`/src/assets/Icons/${props.company}.png`} alt="microsoft"/></div>
                     <div>
                         <div className={'font-semibold'}>{props.jobTitle}</div>
-                        <div className={'text-xs text-mine-shaft-300'}>{props.company} &#x2022; {props.applicants ? props.applicants.length : 0} Applicants</div>
+                        <div className={'text-xs text-mine-shaft-300'}><span className="text-mine-shaft-300 cursor-pointer hover:underline" onClick={(e) => {
+                            e.preventDefault();
+                            e.stopPropagation(); // Prevent parent <Link> navigation
+                            navigate(`/company/${props.company}`);
+                        }}>
+                                {props.company}
+                            </span>  &#x2022; {props.applicants ? props.applicants.length : 0} Applicants</div>
                     </div>
                 </div>
-                <IconBookmark className={'text-mine-shaft-300 cursor-pointer'}/>
+                {profileState?.savedJobs?.includes(props.id) ? <IconBookmarkFilled onClick={(e) => handleSaveJobs(e, props.id)} className={'text-bright-sun-400 cursor-pointer'}
+                               stroke={1.5}/>
+                    :<IconBookmark onClick={(e) => handleSaveJobs(e, props.id)} className={'text-mine-shaft-300 hover:text-bright-sun-400 cursor-pointer'}
+                               stroke={1.5}/>}
             </div>
             <div
                 className={'flex gap-2 [&>div]:py-1 [&>div]:px-2 [&>div]:bg-mine-shaft-800 [&>div]:text-bright-sun-400 [&>div]:rounded-lg text-xs'}>
