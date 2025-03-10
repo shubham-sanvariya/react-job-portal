a4a2c27fae58a85d591f8ef5ea4ab5241cb5a6f6
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-10 22:15:22 +0530

    getting jobs by there status in posted job component

--- src/components/postedjob/postedJob.tsx
+++ src/components/postedjob/postedJob.tsx
@@ -1,30 +1,40 @@
 import {Tabs} from "@mantine/core";
-import CompanyJobs from "@/components/companyProfile/companyJobs.tsx";
-import {activeJobs} from "@/Data/PostedJob.tsx";
 import PostedJobCard from "@/components/postedjob/postedJobCard.tsx";
+import {useSelector} from "react-redux";
+import {useState} from "react";
+import {selectPostedJobs} from "@/slices/postedJobSlice.ts";
 
 const PostedJob = () => {
+
+    const [activeJob, setActiveJob] = useState<string | null>("ACTIVE");
+    const postedJobsState = useSelector(selectPostedJobs);
+
+    const getJobsByStatus = (status: string) => {
+        if (postedJobsState) {
+            return postedJobsState?.filter(job => job.jobStatus === status) ?? [];
+        }
+        return [];
+    }
+
     return (
         <div className={'w-1/6 mt-5'}>
             <div className={'text-2xl font-semibold mb-5'}>
                 Jobs
             </div>
             <div>
-                <Tabs autoContrast variant={"pills"}  defaultValue={"active"}>
+                <Tabs autoContrast variant={"pills"} value={activeJob} onChange={setActiveJob}>
                     <Tabs.List className={'font-medium [&_button[aria-selected="false"]]:bg-mine-shaft-900'}>
-                        <Tabs.Tab value={'active'}>Active</Tabs.Tab>
-                        <Tabs.Tab value={'draft'}>Jobs</Tabs.Tab>
+                        <Tabs.Tab value={'ACTIVE'}>Active {getJobsByStatus("ACTIVE")?.length}</Tabs.Tab>
+                        <Tabs.Tab value={'DRAFT'}>Draft {getJobsByStatus("DRAFT")?.length}</Tabs.Tab>
                     </Tabs.List>
-
-                    <Tabs.Panel value={'active'}>
-                        <div className={'flex flex-col gap-5 mt-5'}>
-                            {activeJobs.map((item,index) => (
-                                <PostedJobCard key={index} {...item}/>
-                            ))}
-                        </div>
-                    </Tabs.Panel>
-                    <Tabs.Panel value={'draft'}><CompanyJobs/></Tabs.Panel>
                 </Tabs>
+
+                <div className={'flex flex-col gap-5 mt-5'}>
+                    {getJobsByStatus(activeJob ?? "").map((item, index) => (
+                        <PostedJobCard key={index} {...item}/>
+                    ))}
+                </div>
+
             </div>
         </div>
     )
