86ef043a2680109256857316604adfe839cbca9c
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-10 22:16:41 +0530

    adding getJobsPostedByUser on getJobsPostedByUser

--- src/pages/postedJobPage.tsx
+++ src/pages/postedJobPage.tsx
@@ -1,13 +1,28 @@
 import PostedJob from "@/components/postedjob/postedJob.tsx";
 import {Divider} from "@mantine/core";
 import PostedJobDesc from "@/components/postedjob/postedJobDesc.tsx";
+import {useDispatch, useSelector} from "react-redux";
+import {AppDispatch} from "@/store.tsx";
+import {selectUser} from "@/slices/userSlice.tsx";
+import {useEffect} from "react";
+import {getPostedByJobsAsyncThunk} from "@/slices/postedJobSlice.ts";
 
 
 const PostedJobPage = () => {
+    const dispatch = useDispatch<AppDispatch>();
+    const userState = useSelector(selectUser);
+
+    useEffect(() => {
+        getJobsPostedByUser()
+    }, [userState]);
+
+    const getJobsPostedByUser = () => {
+        dispatch(getPostedByJobsAsyncThunk(Number(userState.id)));
+    }
     return (
         <div className="min-h-[90vh] bg-mine-shaft-950 font-[poppins] px-4">
             <Divider size={"xs"}/>
-            <div className={'flex gap-5'}>
+            <div className={'flex gap-4'}>
                 <PostedJob/>
                 <PostedJobDesc/>
             </div>
