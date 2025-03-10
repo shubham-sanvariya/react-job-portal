68551db2ea32a4a6e7b79d1cf7220c5f5cc43dc4
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-10 22:15:57 +0530

    adding routing on postedJobCard component

--- src/components/postedjob/postedJobCard.tsx
+++ src/components/postedjob/postedJobCard.tsx
@@ -1,11 +1,15 @@
+import {timeAgo} from "@/services/utilService.tsx";
+import {Link, useParams} from "react-router-dom";
 
 const PostedJobCard = ( props : any) => {
+    const {id} = useParams();
+
     return (
-        <div className={'bg-mine-shaft-900 rounded-xl p-2 border-l-2 border-l-bright-sun-400'}>
+        <Link to={`/posted-jobs/${props.id}`} className={`w-52 rounded-xl p-2 border-l-2 border-l-bright-sun-400 ${props.id == id ? "!bg-bright-sun-400 text-black" : "bg-mine-shaft-900 text-mine-shaft-300"} hover:bg-opacity-80 cursor-pointer`}>
             <div className={'text-sm font-semibold'}>{props.jobTitle}</div>
-            <div className={'text-sm text-mine-shaft-300 font-medium'}>{props.location}</div>
-            <div className={'text-sm text-mine-shaft-300'}>{props.posted}</div>
-        </div>
+            <div className={'text-sm  font-medium'}>{props.location}</div>
+            <div className={'text-sm '}>{timeAgo(props.postTime)}</div>
+        </Link>
     )
 }
 export default PostedJobCard
