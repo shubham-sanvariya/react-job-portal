bfca0e972904ac5667d46e01e5801f3301882ec0
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-12 12:29:10 +0530

    adding catch error Notification

--- src/services/profileService.tsx
+++ src/services/profileService.tsx
@@ -1,16 +1,24 @@
 
 import axios from "axios";
 import {ProfileType} from "@/types/profileType.ts";
+import {errorNotification} from "@/services/notificationServices.tsx";
 
 const base_URL = "http://localhost:8080/profiles"
 
-export const getProfile = async ( id: number) => {
+export const getProfile = async ( id: number, notificationMessage : string) => {
     try {
         const res = await axios.get(`${base_URL}/${id}`);
         return res.data;
     }catch (err : unknown){
-        console.log(err);
-        throw err;
+        let errMsg: string;
+        if (axios.isAxiosError(err) && err.response?.data?.errorMessage) {
+            errMsg = err.response?.data?.errorMessage
+            console.log(errMsg);
+        } else {
+            errMsg = "An unexpected error occurred"
+            console.log(errMsg, err);
+        }
+        errorNotification(notificationMessage, errMsg);
     }
 }
 
@@ -19,8 +27,15 @@ export const updateProfile = async (profile : ProfileType) => {
         const res = await axios.put(`${base_URL}/update`,profile);
         return res.data;
     }catch (err : unknown){
-        console.log(err);
-        throw err;
+        let errMsg: string;
+        if (axios.isAxiosError(err) && err.response?.data?.errorMessage) {
+            errMsg = err.response?.data?.errorMessage
+            console.log(errMsg);
+        } else {
+            errMsg = "An unexpected error occurred"
+            console.log(errMsg, err);
+        }
+        errorNotification("Failed to update Profile", errMsg);
     }
 }
 
@@ -29,7 +44,14 @@ export const updateProfileSavedJobs = async (profileId : number, jobIds : number
         const res = await axios.patch(`${base_URL}/update/saved-jobs/${profileId}`,jobIds);
         return res.data;
     }catch (err : unknown){
-        console.log(err);
-        throw err;
+        let errMsg: string;
+        if (axios.isAxiosError(err) && err.response?.data?.errorMessage) {
+            errMsg = err.response?.data?.errorMessage
+            console.log(errMsg);
+        } else {
+            errMsg = "An unexpected error occurred"
+            console.log(errMsg, err);
+        }
+        errorNotification("Failed to update saved jobs.", errMsg);
     }
 }
