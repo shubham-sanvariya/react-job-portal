ce0b798b7f70773986d4e0b046ff115e92a986ac
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-18 18:40:46 +0530

    removing static data field

--- src/pages/talentProfilePage.tsx
+++ src/pages/talentProfilePage.tsx
@@ -2,13 +2,11 @@ import {Button} from "@mantine/core";
 import {Link, useNavigate} from "react-router-dom";
 import {IconArrowLeft} from "@tabler/icons-react";
 import Profile from "@/components/talentProfile/profile.tsx";
-import {profile} from "@/Data/TalentData.tsx";
 import RecommendTalent from "@/components/talentProfile/recommendTalent.tsx";
 
 
 const TalentProfilePage = () => {
     const navigate = useNavigate();
-
     return (
         <div className="min-h-[90vh] bg-mine-shaft-950 font-[poppins] p-4">
             <Link className={'my-4 inline-block'} to={'/find-talent'}>
@@ -17,7 +15,7 @@ const TalentProfilePage = () => {
                 </Button>
             </Link>
             <div className={'flex gap-5'}>
-                <Profile {...profile}/>
+                <Profile/>
                 <RecommendTalent/>
             </div>
         </div>
