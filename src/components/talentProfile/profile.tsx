056a72da375dead01dbcceb03761cfe39f03b238
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-18 18:39:42 +0530

    adding updating applicant profile component

--- src/components/talentProfile/profile.tsx
+++ src/components/talentProfile/profile.tsx
@@ -1,32 +1,56 @@
 import {IconBriefcase, IconMapPin} from "@tabler/icons-react";
-import {Button, Divider} from "@mantine/core";
+import {Avatar, Button, Divider} from "@mantine/core";
 import ExpCard from "@/components/talentProfile/expCard.tsx";
 import CertiCard from "@/components/talentProfile/certiCard.tsx";
+import {useParams} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {AppDispatch} from "@/store.tsx";
+import {getApplicantProfileAsyncThunk, selectApplicantProfile} from "@/slices/applicantProfile.ts";
+import {useCallback, useEffect} from "react";
+import {CertificationType} from "@/types/profileType.ts";
 
-const Profile = (props: any) => {
-    return (
-        <div className={'w-2/3'}>
+const Profile = () => {
+    const {id} = useParams();
+    const dispatch = useDispatch<AppDispatch>();
+    const profileApplicantState = useSelector(selectApplicantProfile);
+    const getApplicantProfileByApplicantId = useCallback(async () => {
+        if (id) {
+            dispatch(getApplicantProfileAsyncThunk(Number(id)));
+        }
+    }, [id]);
+
+    useEffect(() => {
+        if (profileApplicantState === null) {
+            getApplicantProfileByApplicantId().then();
+        }
+        console.log(profileApplicantState);
+    }, [profileApplicantState]);
+
+
+    return (profileApplicantState &&
+        <div className={'w-2/3 mx-auto'}>
             <div className={'relative'}>
-                <img className={'rounded-t-2xl'} src={`src/assets/Profile/banner.jpg`} alt={""}/>
-                <img
-                    className={'w-48 h-48 mb-10 rounded-full -bottom-1/3 absolute left-3 border-8 border-mine-shaft-950'}
-                    src={`src/assets/avatar.png`} alt={""}/>
+                <img className={'rounded-t-2xl'} src={`https://i0.wp.com/mahala.co.uk/wp-content/uploads/2014/12/img_banner-thin_mountains.jpg?fit=800%2C258&ssl=1`} alt={"banner"}/>
+                <div className={'absolute flex items-center justify-center -bottom-1/4 left-3'}>
+                    <Avatar className="!w-48 !h-48  border-mine-shaft-950 border-8 rounded-full"
+                            src={profileApplicantState?.picture ? `data:image/jpeg;base64,${profileApplicantState.picture}` : 'src/assets/avatar.png'}/>
+                </div>
             </div>
             <div className={'px-3 mt-16'}>
-                <div className={'flex justify-between text-3xl font-semibold '}>{props.name}<Button
+                <div className={'flex justify-between text-3xl font-semibold '}>{profileApplicantState.name}<Button
                     color={'bright-sun.4'} variant={"light"}>Message</Button></div>
                 <div className={'text-xl flex gap-1 items-center'}><IconBriefcase
-                    className={'h-5 w-5'}/> {props.role} &bull; {props.company}
+                    className={'h-5 w-5'}/> {profileApplicantState.jobTitle} &bull; {profileApplicantState.company}
                 </div>
                 <div className={'flex gap-1 text-xs text-mine-shaft-400 items-center'}>
-                    <IconMapPin className={'h-5 w-5'} stroke={1.5}/> {props.location}
+                    <IconMapPin className={'h-5 w-5'} stroke={1.5}/> {profileApplicantState.location}
                 </div>
             </div>
             <Divider mx={'xs'} my={"xl"}/>
             <div className={'px-3'}>
                 <div className={'text-2xl font-semibold mb-3'}>About</div>
                 <div className={'text-sm text-mine-shaft-300 text-justify'}>
-                    {props.about}
+                    {profileApplicantState.about}
                 </div>
             </div>
             <Divider mx={'xs'} my={"xl"}/>
@@ -34,7 +58,7 @@ const Profile = (props: any) => {
                 <div className={'text-2xl font-semibold mb-3'}>Skills</div>
                 <div className={'flex flex-wrap gap-2'}>
                     {
-                        props.skills.map((skill: string, index: number) => (
+                        profileApplicantState.skills.map((skill: string, index: number) => (
                             <div key={index}
                                  className={'bg-bright-sun-300 font-medium fow bg-opacity-15 rounded-3xl text-bright-sun-400 px-3 py-1'}>
                                 {skill}
@@ -48,7 +72,7 @@ const Profile = (props: any) => {
                 <div className={'text-2xl font-semibold mb-5'}>Experience</div>
                 <div className={'flex flex-col gap-8'}>
                     {
-                        props.experience.map((exp: object, index: number) => (
+                        profileApplicantState.experiences.map((exp: object, index: number) => (
                             <ExpCard key={index} {...exp}/>
                         ))
                     }
@@ -59,7 +83,7 @@ const Profile = (props: any) => {
                 <div className={'text-2xl font-semibold mb-5'}>Certifications</div>
                 <div className={'flex flex-col gap-8'}>
                     {
-                        props.certifications.map((certi: object, index: number) => (
+                        profileApplicantState.certificates.map((certi: CertificationType, index: number) => (
                             <CertiCard key={index} {...certi}/>
                         ))
                     }
