862c3806e4230b1eeaf614ceaa5d0b8762501bd5
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-19 00:17:04 +0530

    adding profiles data in recommendTalent component

--- src/components/talentProfile/recommendTalent.tsx
+++ src/components/talentProfile/recommendTalent.tsx
@@ -1,7 +1,26 @@
-import {talents} from "@/Data/TalentData.tsx";
 import TalentCard from "@/components/findTalent/talentCard.tsx";
+import {useParams} from "react-router-dom";
+import {useCallback, useEffect, useState} from "react";
+import {ProfileType} from "@/types/profileType.ts";
+import {getAllProfiles} from "@/services/profileService.tsx";
 
 const RecommendTalent = () => {
+    const {id} = useParams();
+    const [profiles, setProfiles] = useState<ProfileType[]>();
+
+    const getAllApplicantProfiles = useCallback(
+        async () => {
+            const profilesData = await getAllProfiles();
+            const filteredProfiles = profilesData?.filter(pro => pro.id !== Number(id));
+            setProfiles(filteredProfiles);
+        },
+        [id],
+    );
+
+    useEffect(() => {
+        getAllApplicantProfiles().then()
+    }, []);
+
     return (
         <div>
             <div className={'text-xl font-semibold mb-5'}>
@@ -9,8 +28,8 @@ const RecommendTalent = () => {
             </div>
             <div className={'flex flex-col flex-wrap justify-between gap-5 '}>
                 {
-                    talents.map((talent,index) => index < 4 && (
-                        <TalentCard key={index} {...talent}/>
+                    profiles?.map((talent,index) => index < 4 && (
+                        <TalentCard key={index} applicantProfile={talent} />
                     ))
                 }
             </div>
