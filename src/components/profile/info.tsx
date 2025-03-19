3e1b6b303bcb96554fc1b946cea8f01cdbb03036
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-19 16:41:32 +0530

    adding name attribute update

--- src/components/profile/info.tsx
+++ src/components/profile/info.tsx
@@ -4,7 +4,7 @@ import SelectInput from "@/components/profile/selectInput.tsx";
 import fields from "@/Data/Profile.tsx";
 import {useDispatch, useSelector} from "react-redux";
 import {selectProfile, updateProfileAsyncThunk} from "@/slices/profileSlice.tsx";
-import {useState} from "react";
+import {useMemo, useState} from "react";
 import {hasLength, useForm} from "@mantine/form";
 import {selectUser} from "@/slices/userSlice.tsx";
 import {ProfileType} from "@/types/profileType.ts";
@@ -17,10 +17,12 @@ const Info = () => {
     const userState = useSelector(selectUser);
     const [edit, setEdit] = useState<boolean>(false);
 
+
     const handleEdit = () => {
         if (!edit) {
             setEdit(true)
             form.setValues({
+                name: userState.name,
                 jobTitle: profileState?.jobTitle,
                 company: profileState?.company,
                 location: profileState?.location
@@ -29,30 +31,39 @@ const Info = () => {
     }
 
     const handleSave = () => {
-        const updatedProfile = {...profileState, ...form.getValues()};
-        dispatch(updateProfileAsyncThunk(updatedProfile as ProfileType));
-        successNotification("Success", "Profile Updated Successfully.")
+        if (isFormValid){
+            console.log("Form values before dispatch:", form.getValues());
+            const updatedProfile = {...profileState, ...form.getValues()};
+            dispatch(updateProfileAsyncThunk(updatedProfile as ProfileType));
+            successNotification("Success", "Profile Updated Successfully.")
+            setEdit(false);
+        }
     }
 
     const form = useForm({
         mode: "controlled",
-        initialValues: {jobTitle: "", company: "", location: ""},
+        initialValues: {name : "",jobTitle: "", company: "", location: ""},
         validate: {
+            name: hasLength({min: 3, max: 100 },'Characters must be between 3 to 100'),
             jobTitle: hasLength({min: 3}, 'Must be at least 3 Characters')
             , company: hasLength({min: 3}, 'Must be at least 3 Characters')
             , location: hasLength({min: 3}, 'Must be at least 3 Characters'),
-        }
+        },
+        validateInputOnChange: true
     })
+
+    const isFormValid = useMemo(() => !form.validate().hasErrors, [form.values]);
+
 
     return (
         <>
-            <div className={'flex justify-between text-3xl font-semibold '}>{userState.name}
+            <div className={'flex justify-between text-3xl font-semibold '}>{profileState?.name}
                 <div>
 
                     <ActionIcon size={"lg"} color={'green.8'} variant={'subtle'}
                                 onClick={handleSave}
                     >
-                        {edit && <IconCheck className={'h-4/5 w-4/5'}/>}
+                        {edit && isFormValid && <IconCheck className={'h-4/5 w-4/5'}/>}
                     </ActionIcon>
                     <ActionIcon size={"lg"} color={edit ? 'red.8':'bright-sun.4'} variant={'subtle'}
                                 onClick={handleEdit}
@@ -63,11 +74,12 @@ const Info = () => {
                 </div>
             </div>
             {edit ? <>
+                    <SelectInput form={form} name={"name"} {...fields[0]}/>
                     <div className={'flex gap-10 [&>*]:w-1/2'}>
-                        <SelectInput form={form} name={"jobTitle"} {...fields[0]} />
-                        <SelectInput form={form} name={"company"} {...fields[1]} />
+                        <SelectInput form={form} name={"jobTitle"} {...fields[1]} />
+                        <SelectInput form={form} name={"company"} {...fields[2]} />
                     </div>
-                    <SelectInput form={form} name={"location"} {...fields[2]} />
+                    <SelectInput form={form} name={"location"} {...fields[3]} />
                 </>
                 :
                 <>
