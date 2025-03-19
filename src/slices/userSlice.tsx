019797dc2ea4f38a56dea94978b36a04512c1c98
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-19 19:27:58 +0530

    Implement async thunk for updating user name and refactor user slice state management

--- src/slices/userSlice.tsx
+++ src/slices/userSlice.tsx
@@ -1,5 +1,6 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import {getItem, removeItem, setItem} from "@/services/localStorageService.tsx";
+import {updateUserName} from "@/services/userService.tsx";
 
 interface User {
     id: string;
@@ -9,11 +10,31 @@ interface User {
     profileId : number;
 }
 
-const initialState : User | null = getItem("user") || null;
+interface UserStateType {
+    user : User | null;
+    loading : boolean;
+    error : string | null;
+}
+
+const initialState: UserStateType = {
+    user: getItem("user") ? getItem("user") : null,
+    loading: false,
+    error: null,
+};
+
+export const updateUserNameAsyncThunk = createAsyncThunk("updateUserName", async ({ id, userName }: {id : number, userName : string},{ rejectWithValue, dispatch })=> {
+    try {
+        const data = await updateUserName(id,userName);
+        if (data) dispatch(setUser(data));
+        return data;
+    }catch (errMessage : unknown){
+        return rejectWithValue(errMessage);
+    }
+})
 
 
 const UserSlice = createSlice({
-    name: "user",
+    name: "userReducer",
     initialState,
     reducers: {
         setUser : (state, action) => {
@@ -25,18 +46,19 @@ const UserSlice = createSlice({
                 accountType: user.accountType,
                 profileId : user.profileId
             });
-            state = getItem("user");
+            state.user = getItem("user");
             return state;
         },
-        removeUser : () => {
+        removeUser : (state) => {
             removeItem("user");
-            return null;
+            state.user = null;
+            return state;
         }
-    }
+    },
 })
 
 export const {setUser, removeUser} = UserSlice.actions;
 
-export const selectUser = (state : {user : User}) => state.user;
+export const selectUser = (state : { userReducer : { user : User }}) => state.userReducer.user;
 
 export default UserSlice.reducer;
