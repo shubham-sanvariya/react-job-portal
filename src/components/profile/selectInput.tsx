6ceb6363558911e99b89c3d25b11b7ce1d9e5ea5
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-19 16:43:07 +0530

    adding check for name attribute

--- src/components/profile/selectInput.tsx
+++ src/components/profile/selectInput.tsx
@@ -3,10 +3,10 @@ import {Combobox, InputBase, ScrollArea, useCombobox} from '@mantine/core';
 
 const SelectInput = (props: any) => {
     useEffect(() => {
-        setData(props.options);
+        if (props.name !== "name") setData(props.options);
         setValue(props.form.getInputProps(props.name).value);
         setSearch(props.form.getInputProps(props.name).value);
-    }, []);
+    }, [props.form, props.name, props.options]);
     const combobox = useCombobox({
         onDropdownClose: () => combobox.resetSelectedOption(),
     });
@@ -14,27 +14,34 @@ const SelectInput = (props: any) => {
     const [data, setData] = useState<string[]>([]);
     const [value, setValue] = useState<string | null>(null);
     const [search, setSearch] = useState('');
+    let options;
+    let exactOptionMatch;
+    if (props.name !== "name"){
+         exactOptionMatch = data?.some((item) => item === search);
+        const filteredOptions = exactOptionMatch
+            ? data
+            : data?.filter((item) => item.toLowerCase().includes(search?.toLowerCase().trim()));
 
-    const exactOptionMatch = data.some((item) => item === search);
-    const filteredOptions = exactOptionMatch
-        ? data
-        : data.filter((item) => item.toLowerCase().includes(search?.toLowerCase().trim()));
+        options = filteredOptions?.map((item) => (
+            <Combobox.Option value={item} key={item}>
+                {item}
+            </Combobox.Option>
+        ));
+    }
 
-    const options = filteredOptions.map((item) => (
-        <Combobox.Option value={item} key={item}>
-            {item}
-        </Combobox.Option>
-    ));
 
     return (
         <Combobox
             store={combobox}
             withinPortal={false}
             onOptionSubmit={(val) => {
+                console.log("onOptionSubmit triggered with value:", val);
                 if (val === '$create') {
-                    setData((current) => [...current, search]);
-                    setValue(search);
-                    props.form.setFieldValue(props.name, search)
+                    const trimmed = search.trim();
+                    setData((current) => [...current, trimmed]);
+                    setValue(trimmed);
+                    console.log("in create",search);
+                    props.form.setFieldValue(props.name, trimmed);
                 } else {
                     setValue(val);
                     setSearch(val);
