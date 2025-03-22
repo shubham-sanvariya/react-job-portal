980041e116e7dddc0f6ced7e5d69034f587ad5ee
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-22 19:38:05 +0530

    Refactor PostedJobCard component to use useMemo for job status determination, enhancing performance and clarity. Update displayed information to include job status alongside the time since posting.

--- src/components/postedjob/postedJobCard.tsx
+++ src/components/postedjob/postedJobCard.tsx
@@ -1,14 +1,23 @@
 import {timeAgo} from "@/services/utilService.tsx";
 import {Link, useParams} from "react-router-dom";
+import {useMemo} from "react";
 
 const PostedJobCard = ( props : any) => {
     const {id} = useParams();
+
+    const status = useMemo(() => {
+        if (props.jobStatus === "DRAFT"){
+            return "Drafted"
+        }else if (props.jobStatus === "CLOSED"){
+            return "Closed"
+        }else return "Published"
+    },[props.jobStatus])
 
     return (
         <Link to={`/posted-jobs/${props.id}`} className={`w-52 rounded-xl p-2 border-l-2 border-l-bright-sun-400 ${props.id == id ? "!bg-bright-sun-400 text-black" : "bg-mine-shaft-900 text-mine-shaft-300"} hover:bg-opacity-80 cursor-pointer`}>
             <div className={'text-sm font-semibold'}>{props.jobTitle}</div>
             <div className={'text-sm  font-medium'}>{props.location}</div>
-            <div className={'text-sm '}>{timeAgo(props.postTime)}</div>
+            <div className={'text-sm '}>{status} {timeAgo(props.postTime)}</div>
         </Link>
     )
 }
