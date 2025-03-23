cd778ce72a8f9b22b9f337dbaae7467cdca7f918
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-23 23:46:08 +0530

    Add filterSlice for managing field filters with update and reset actions, enhancing dynamic filtering capabilities in the Redux store.

---
+++ src/slices/filterSlice.ts
@@ -0,0 +1,41 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+interface FieldsState{
+    [key : string] : string[] | number[] | string
+}
+
+const initialState : FieldsState = {}
+
+const filterSlice = createSlice({
+    initialState,
+    name: "filterFieldsReducer",
+    reducers: {
+        updateFieldFilter: (state, action : PayloadAction<{ key : string, value : string | number[]}>) => {
+            const { key, value } = action.payload;
+            if (key === "name"){
+                state[key] = value;
+            }
+            else if (typeof value === "string"){
+                if (!state[key]){
+                    state[key] = key === "name" ? value : [value];
+                } else {
+                    const stringArray = state[key] as string[];
+                    state[key] = stringArray.includes(value) ?
+                        stringArray.filter(item => item !== value) :
+                        [...stringArray, value]
+                }
+            } else if (Array.isArray(value)) {
+                state[key] = value
+            }
+            console.log(JSON.parse(JSON.stringify(state)));
+        },
+        // updateExpRange: (state, action : PayloadAction<>),
+        resetFieldFilter : () => initialState
+    },
+})
+
+export const { updateFieldFilter, resetFieldFilter } = filterSlice.actions;
+
+export const selectFilteredFieldState = (state : {filterFieldsReducer: FieldsState  }) => state.filterFieldsReducer
+
+export default filterSlice.reducer;
