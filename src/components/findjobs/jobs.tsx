098639573db39bd54e7166687e052832e020a27c
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-03-25 23:20:03 +0530

    Implement job filtering logic in Jobs component using Redux state, enhancing job display based on user-defined criteria and ensuring cleanup of field filters on unmount.

--- src/components/findjobs/jobs.tsx
+++ src/components/findjobs/jobs.tsx
@@ -3,17 +3,46 @@ import JobCard from "@/components/findjobs/jobCard.tsx";
 import {useDispatch, useSelector} from "react-redux";
 import {AppDispatch} from "@/store.tsx";
 import {getJobsAsyncThunk, selectJobs} from "@/slices/jobSlice.ts";
-import {useEffect} from "react";
+import {useEffect, useMemo, useRef} from "react";
+import {resetFieldFilter, selectFilteredFieldState} from "@/slices/filterSlice.ts";
+import {JobType} from "@/types/jobType.ts";
 
 const Jobs = () => {
     const dispatch = useDispatch<AppDispatch>();
     const jobsState = useSelector(selectJobs);
+    const filteredFields = useSelector(selectFilteredFieldState);
+    const oneRef = useRef(false);
 
     useEffect(() => {
-        dispatch(getJobsAsyncThunk());
-        console.log(jobsState)
-
+        if (jobsState?.length === 0 && !oneRef.current) {
+            dispatch(getJobsAsyncThunk());
+            oneRef.current = true;
+        }
+        return () => {
+            dispatch(resetFieldFilter());
+        }
     }, []);
+
+
+    const filteredJobs = useMemo(() => {
+        if (!filteredFields && typeof filteredFields !== "object") return jobsState;
+        const keys = Object.keys(filteredFields);
+        const check = new Array(keys.length).fill(false);
+
+        return jobsState?.filter(job => {
+            keys.forEach((key, index) => {
+                if (Array.isArray(filteredFields[key]) && typeof filteredFields[key][0] === "string"){
+                    const stringArray = filteredFields[key] as string[];
+                    check[index] = stringArray.some((val : string) => val === job[key as keyof JobType]);
+                }else if (Array.isArray(filteredFields[key]) && typeof filteredFields[key][0] === "number"){
+                    const numberField = filteredFields[key] as number[];
+                    check[index] = numberField[0] <= job.packageOffered && numberField[1] >= job.packageOffered
+                }
+            })
+            return check.every(val => val === true);
+        })
+    }, [filteredFields, jobsState])
+
     return (
         <div className={'p-5'}>
             <div className={'flex justify-between'}>
@@ -22,7 +51,7 @@ const Jobs = () => {
             </div>
             <div className={'flex flex-wrap mt-10 gap-5'}>
                 {
-                    jobsState?.map((job, index) => (
+                    filteredJobs?.map((job, index) => (
                         <JobCard key={index} {...job}/>
                     ))
                 }
