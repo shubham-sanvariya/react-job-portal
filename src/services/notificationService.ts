65330f96268e423443b4614a23a2e649d2dada52
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-04 17:10:40 +0530

    Add getAllNotificationByUserId function to notificationService for fetching unread notifications by user ID, including error handling and notification display on failure.

---
+++ src/services/notificationService.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import {errorNotification} from "@/services/notificationServices.tsx";
+
+const BASE_URL = "http://localhost:8080/notifications"
+
+export const getAllNotificationByUserId = async (userId: number) => {
+    try {
+        const res = await axios.get(`${BASE_URL}/unread/${userId}`);
+        return res.data;
+    } catch (err: unknown) {
+        let errMsg: string;
+        if (axios.isAxiosError(err) && err.response?.data?.errorMessage) {
+            errMsg = err.response?.data?.errorMessage
+            console.log(errMsg);
+        } else {
+            errMsg = "An unexpected error occurred"
+            console.log(errMsg, err);
+        }
+        errorNotification("Failed to fetch notifications", errMsg);
+    }
+}
