347f7f21df81ee9aaa8f0ec6b07c60c963c9fc19
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-04 17:10:49 +0530

    Add NotificationMenu component with notification fetching logic, including state management for displaying notifications and handling unread notifications.

---
+++ src/components/Header/notificationMenu.tsx
@@ -0,0 +1,75 @@
+import {Indicator, Menu, Notification, rem} from "@mantine/core";
+import {
+    IconBell, IconCheck
+} from "@tabler/icons-react";
+import {useEffect, useRef, useState} from "react";
+import {getAllNotificationByUserId} from "@/services/notificationService.ts";
+import {selectUser} from "@/slices/userSlice.tsx";
+import {useSelector} from "react-redux";
+import {NotificationType} from "@/types/notificationType.ts";
+
+const NotificationMenu = () => {
+    const userState = useSelector(selectUser);
+    const [opened, setOpened] = useState(false);
+    const [notifications, setNotifications] = useState<NotificationType[]>([]);
+    const ref = useRef(false);
+
+    useEffect(() => {
+        if (!ref.current) {
+            getAllNotificationByUserId(Number(userState.id))
+                .then(res => {
+                    console.log(res);
+                    setNotifications(res as NotificationType[]);
+                }).catch(err => console.log(err)
+            )
+            ref.current = true;
+        }
+    }, [userState.id]);
+
+    const unRead = (removeIndex : number) => {
+        setNotifications((values) => {
+            const res= values.filter((_,index) => index !== removeIndex);
+            console.log(res);
+            return res;
+        })
+    }
+
+    return (
+        <Menu opened={opened} onChange={setOpened} shadow="md" width={400}>
+            <Menu.Target>
+                <div className={'bg-mine-shaft-900 p-1.5 rounded-full'}>
+                    <Indicator color={'bright-sun.4'} offset={6} size={7} processing>
+                        <IconBell stroke={'1.5'}/>
+                    </Indicator>
+                </div>
+            </Menu.Target>
+
+            <Menu.Dropdown onChange={() => setOpened(true)}>
+                <div className={'flex flex-col gap-1'}>
+                    {
+                        notifications.map((item : NotificationType, index : number) => (
+                            <Notification tabIndex={index} onClose={() => unRead(index)} className={'cursor-pointer hover:bg-mine-shaft-900'}
+                                          icon={<IconCheck style={{width: rem(20), height: rem(20)}}/>} color="teal"
+                                          title={item.action} mt="md">
+                                {item.message}
+                            </Notification>
+                        ))
+                    }
+                    {
+                        notifications.length === 0 && (
+                            <div className="text-center text-mine-shaft-300">
+                                No Notifications
+                            </div>
+                        )
+                    }
+                </div>
+
+
+                <Menu.Divider/>
+
+
+            </Menu.Dropdown>
+        </Menu>
+    )
+}
+export default NotificationMenu
