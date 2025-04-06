602432857aff7e3dbc36edc063e867c293f43905
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-06 16:36:57 +0530

    Remove unused user authentication functions from userService, streamlining the service to only export changePassword and updateUserName.

--- src/services/userService.tsx
+++ src/services/userService.tsx
@@ -3,45 +3,6 @@ import {errorNotification} from "@/services/notificationServices.tsx";
 
 const base_URL = "http://localhost:8080/users"
 
-const registerUser = async ( user: any ) => {
-    try {
-        const response = await axios.post(`${base_URL}/register`, user);
-        return response.data;
-    } catch (error : unknown) {
-        console.log(error);
-        throw error;
-    }
-}
-
-const loginUser = async ( login: any ) => {
-    try {
-        const response = await axios.post(`${base_URL}/login`, login);
-        return response.data;
-    } catch (error : unknown) {
-        console.log(error);
-        throw error;
-    }
-}
-
-const sendOtp = async ( email : string) => {
-    try {
-        const res = await axios.post(`${base_URL}/sendOtp/${email}`);
-        return res.data;
-    }catch (error : unknown) {
-        console.log(error);
-        throw error;
-    }
-}
-
-const verifyOtp = async ( email : string, otp : string) => {
-    try {
-        const res = await axios.get(`${base_URL}/verifyOtp/${email}/${otp}`);
-        return res.data;
-    }catch (error : unknown) {
-        console.log(error);
-        throw error;
-    }
-}
 
 const changePassword = async ( email : string, password : string) => {
     try {
@@ -71,4 +32,4 @@ const updateUserName = async ( id : number, newUsername : string) => {
     }
 }
 
-export {registerUser, loginUser, sendOtp, verifyOtp, changePassword, updateUserName};
+export { changePassword, updateUserName};
