3b6f129b4486980189e3dff3e3ea41b560325643
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-08 01:32:51 +0530

    Update NavLinks component to conditionally render links based on user account type, enhancing navigation for applicants and employers. Added Job History link for applicants.

--- src/components/Header/navLinks.tsx
+++ src/components/Header/navLinks.tsx
@@ -1,21 +1,25 @@
 import {Link, useLocation} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {selectUser} from "@/slices/userSlice.tsx";
 
 const NavLinks = () => {
-
-    const links = [
+    const userState = useSelector(selectUser);
+    const links = userState.accountType === "APPLICANT" ? [
         {name: "Find Jobs", url: "find-jobs"},
+        {name: "Job History", url: "job-history"},
+    ] : [
         {name: "Find Talent", url: "find-talent"},
         {name: "Post Jobs", url: "post-job"},
         {name: "Posted Job", url: "posted-jobs"},
-        {name: "Job History", url: "job-history"},
     ]
 
     const location = useLocation();
 
     return (
         <div className={'flex items-center gap-5 h-full  text-mine-shaft-300'}>
-            {links.map((link,index) => (
-                <div key={index} className={`${location.pathname=="/"+link.url ? "border-bright-sun-400 text-bright-sun-400" : "border-transparent"} flex items-center border-t-[3px] h-full`}>
+            {links.map((link, index) => (
+                <div key={index}
+                     className={`${location.pathname == "/" + link.url ? "border-bright-sun-400 text-bright-sun-400" : "border-transparent"} flex items-center border-t-[3px] h-full`}>
                     <Link to={`/${link.url}`}>
                         {link.name}
                     </Link>
