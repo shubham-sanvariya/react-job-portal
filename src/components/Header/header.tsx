6dddb1f5ec915df1eae61123858e743a3e0b116f
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-10 22:38:57 +0530

    Add Drawer and Burger components to Header for improved navigation. Integrated useDisclosure for managing drawer state and utilized useNavLinks for dynamic link rendering, enhancing user experience and accessibility.

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,4 +1,4 @@
-import {Button} from "@mantine/core";
+import {Burger, Button, Drawer} from "@mantine/core";
 import NavLinks from "@/components/Header/navLinks.tsx";
 import {Link, useLocation, useNavigate} from "react-router-dom";
 import ProfileMenu from "@/components/Header/profileMenu.tsx";
@@ -8,12 +8,17 @@ import {AppDispatch} from "@/store.tsx";
 import {getProfileAsyncThunk} from "@/slices/profileSlice.tsx";
 import {useEffect, useRef} from "react";
 import NotificationMenu from "@/components/Header/notificationMenu.tsx";
+import {useDisclosure} from "@mantine/hooks";
+import {IconX} from "@tabler/icons-react";
+import useNavLinks from "@/hooks/useNavLinks.tsx";
 
 const Header = () => {
     const dispatch = useDispatch<AppDispatch>();
     const navigate = useNavigate();
     const user = useSelector(selectUser);
     const ref = useRef(false);
+    const [opened, { open, close }] = useDisclosure(false);
+    const { links } = useNavLinks();
 
     useEffect(() => {
         if (user?.profileId && !ref.current) {
@@ -41,6 +46,18 @@ const Header = () => {
                 {/*<div className={'bg-mine-shaft-900 p-1.5 rounded-full'}>*/}
                 {/*    <IconSettings stroke={'1.5'}/>*/}
                 {/*</div>*/}
+
+                <Burger opened={opened} onClick={open} aria-label="Toggle navigation" />
+                <Drawer size={"xs"} overlayProps={{ backgroundOpacity : 0.5 , blur: 4}} opened={opened} onClose={close} position={"right"} title="Authentication" closeButtonProps={{ icon : <IconX size={30} />}}>
+                    {links.map((link, index) => (
+                        <div key={index}
+                             className={`${location.pathname == "/" + link.url ? "border-bright-sun-400 text-bright-sun-400" : "border-transparent"} flex items-center border-t-[3px] h-full`}>
+                            <Link to={`/${link.url}`}>
+                                {link.name}
+                            </Link>
+                        </div>
+                    ))}
+                </Drawer>
             </div>
         </div>
     ) : <></>
