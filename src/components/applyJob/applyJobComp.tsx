b6eb02c7d6fcae299281c1337a4e3679922eb811
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-14 16:17:08 +0530

    Enhance ApplyJobComp layout by adding responsive utility classes for better alignment and spacing on smaller screens, improving overall user experience.

--- src/components/applyJob/applyJobComp.tsx
+++ src/components/applyJob/applyJobComp.tsx
@@ -33,15 +33,15 @@ const ApplyJobComp = () => {
     }, [dispatch, id, memoizedJobs]);
 
     return (<>
-            <div className={'w-2/3 mx-auto'}>
+            <div className={'w-2/3 mx-auto bs-mx:w-4/5 sm-mx:w-full'}>
 
                 <div className={'flex justify-between'}>
                     <div className={'flex items-center gap-2'}>
-                        <div className={'p-3 bg-mine-shaft-800 rounded-xl'}><img className={'h-14'}
+                        <div className={'p-3 bg-mine-shaft-800 rounded-xl shrink-0'}><img className={'h-14 xs-mx:h-10 xs-mx:w-10'}
                                                                                  src={`/src/assets/Icons/${job.company}.png`}
                                                                                  alt="microsoft"/></div>
                         <div>
-                            <div className={'font-semibold text-2xl'}>{job.jobTitle}</div>
+                            <div className={'font-semibold text-2xl xs-mx:text-xl'}>{job.jobTitle}</div>
                             <div
                                 className={'text-lg text-mine-shaft-300'}>{job?.company} &bull; {timeAgo(job?.postTime ?? "")} &bull;
                                 &nbsp;
