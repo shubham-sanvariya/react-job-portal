35b598b7a3ab561ad000ceb78ea46d2b0abf07b2
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-14 23:53:08 +0530

    Enhance Profile component by adding responsive utility classes and media queries for improved layout on various screen sizes, ensuring a better user experience across devices.

--- src/components/talentProfile/profile.tsx
+++ src/components/talentProfile/profile.tsx
@@ -9,11 +9,14 @@ import {getApplicantProfileAsyncThunk, selectApplicantProfile} from "@/slices/ap
 import {useCallback, useEffect} from "react";
 import {CertificationType} from "@/types/profileType.ts";
 import banner from '@/assets/Profile/banner.jpg';
+import {useMediaQuery} from "@mantine/hooks";
 
 const Profile = () => {
     const {id} = useParams();
     const dispatch = useDispatch<AppDispatch>();
     const profileApplicantState = useSelector(selectApplicantProfile);
+    const matches = useMediaQuery('(max-width: 475px)');
+
     const getApplicantProfileByApplicantId = useCallback(async () => {
         if (id) {
             dispatch(getApplicantProfileAsyncThunk(Number(id)));
@@ -27,24 +30,25 @@ const Profile = () => {
 
 
     return (profileApplicantState &&
-        <div className={'w-2/3 mx-auto'}>
+        <div className={'w-2/3 mx-auto lg-mx:w-full'}>
             <div className={'relative'}>
-                <img className={'rounded-t-2xl'} src={banner} alt={"banner"}/>
-                <div className={'absolute flex items-center justify-center -bottom-1/4 left-3'}>
-                    <Avatar className="!w-48 !h-48  border-mine-shaft-950 border-8 rounded-full"
+                <img className={'rounded-t-2xl xs-mx:!h-32 xl-mx:h-40 w-full'} src={banner} alt={"banner"}/>
+                <div className={'absolute flex items-center justify-center -bottom-1/4 left-6 md-mx:-bottom-10 sm-mx:-bottom-16'}>
+                    <Avatar className="!w-48 !h-48  border-mine-shaft-950 border-8 rounded-full md-mx:!w-40 md-mx:!h-40 sm-mx:!w-36 sm-mx:!h-36 xs-mx:!w-32 xs-mx:!h-32 "
                             src={profileApplicantState?.picture ? `data:image/jpeg;base64,${profileApplicantState.picture}` : 'src/assets/avatar.png'}/>
                 </div>
             </div>
             <div className={'px-3 mt-16'}>
-                <div className={'flex justify-between text-3xl font-semibold '}>{profileApplicantState.name}<Button
+                <div className={'flex justify-between text-3xl font-semibold xs-mx:text-2xl'}>{profileApplicantState.name}<Button
+                    size={matches? "sm": "md"}
                     color={'bright-sun.4'} variant={"light"}>Message</Button></div>
-                <div className={'text-xl flex gap-1 items-center'}><IconBriefcase
+                <div className={'text-xl flex gap-1 items-center xs-mx:text-base'}><IconBriefcase
                     className={'h-5 w-5'}/> {profileApplicantState.jobTitle} &bull; {profileApplicantState.company}
                 </div>
-                <div className={'flex gap-1 text-lg text-mine-shaft-400 items-center'}>
+                <div className={'flex gap-1 text-lg text-mine-shaft-400 items-center xs-mx:text-base'}>
                     <IconMapPin className={'h-5 w-5'} stroke={1.5}/> {profileApplicantState.location}
                 </div>
-                <div className={'flex gap-1 text-lg text-mine-shaft-400 items-center'}>
+                <div className={'flex gap-1 text-lg text-mine-shaft-400 items-center xs-mx:text-base'}>
                     <IconBriefcase className={'h-5 w-5'} stroke={1.5}/> Experience: {profileApplicantState.totalExperience} {profileApplicantState.totalExperience > 1 ? "years" : "year"}
                 </div>
             </div>
