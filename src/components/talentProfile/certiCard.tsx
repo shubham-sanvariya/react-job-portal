8eaf0a9bbcbbc912184412c2d80c38c237ce2cb4
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-14 23:52:49 +0530

    Refactor CertiCard component to improve responsiveness by adjusting utility classes and adding a formatted issue date label, enhancing clarity and user experience across devices.

--- src/components/talentProfile/certiCard.tsx
+++ src/components/talentProfile/certiCard.tsx
@@ -1,19 +1,20 @@
 import {CertificationType} from "@/types/profileType.ts";
+import {formatDate} from "@/services/utilService.tsx";
 
 const CertiCard = (props:CertificationType) => {
     return (
 
-        <div className={'flex justify-between'}>
+        <div className={'flex justify-between sm-mx:flex-wrap sm-mx:gap-2'}>
             <div className={'flex items-center gap-2'}>
-                <div className={'p-2 bg-mine-shaft-800 rounded-md'}><img className={'h-7'} src={`/src/assets/Icons/${props.issuer}.png`} alt="microsoft"/></div>
+                <div className={'p-2 bg-mine-shaft-800 rounded-md shrink-0'}><img className={'h-7'} src={`/src/assets/Icons/${props.issuer}.png`} alt="microsoft"/></div>
                 <div className={'flex flex-col'}>
-                    <div className={'font-semibold'}>{props.title}</div>
-                    <div className={'text-sm text-mine-shaft-300'}>{props.issuer}</div>
+                    <div className={'font-semibold xs-mx:text-sm'}>{props.title}</div>
+                    <div className={'text-sm text-mine-shaft-300 xs-mx:text-sm'}>{props.issuer}</div>
                 </div>
             </div>
-            <div className={'flex flex-col items-end'}>
-                <div className={'text-sm text-mine-shaft-300'}>{props.issueDate}</div>
-                <div className={'text-sm text-mine-shaft-300'}>ID: {props.certificateId}</div>
+            <div className={'flex flex-col items-end sm-mx:flex-row sm-mx:gap-2'}>
+                <div className={'text-sm text-mine-shaft-300'}>Issued {formatDate(props.issueDate)}</div>
+                <div className={'text-sm text-mine-shaft-300'}>ID: &nbsp; {props.certificateId}</div>
             </div>
         </div>
     )
