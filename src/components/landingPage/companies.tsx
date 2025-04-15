14239ab47295853078a93e8ffbd7a9538c0f9b09
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-15 19:48:41 +0530

    Enhance Companies component by adding responsive utility classes for improved layout across various screen sizes, ensuring a better user experience on mobile and desktop devices.

--- src/components/landingPage/companies.tsx
+++ src/components/landingPage/companies.tsx
@@ -4,7 +4,7 @@ import {companies} from "@/Data/Data.tsx";
 const Companies = () => {
     return (
         <div className={'mt-20 pb-5'}>
-            <div className={'text-4xl text-center font-semibold text-mine-shaft-100 mb-10'}>
+            <div className={'text-4xl text-center font-semibold text-mine-shaft-100 mb-10 md-mx:text-3xl sm-mx:text-2xl xs-mx:text-xl'}>
                 Trusted By <span className={'text-bright-sun-400'}>
                 1000+
             </span> Companies
@@ -13,7 +13,7 @@ const Companies = () => {
             <Marquee pauseOnHover={true}>
                 {
                     companies.map((com,index) => (
-                        <div key={index} className={'mx-8 px-2 py-1 hover:bg-mine-shaft-900 rounded-xl cursor-pointer'}>
+                        <div key={index} className={'mx-8 px-2 py-1 hover:bg-mine-shaft-900 rounded-xl cursor-pointer sm-mx:mx-6 xs-mx:mx-4 xsm-mx:2'}>
                             <img className={'h-14'} src={`/src/assets/Companies/${com}.png`} alt={com}/>
                         </div>
                     ))
