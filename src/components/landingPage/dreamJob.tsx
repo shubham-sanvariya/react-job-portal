c4ece2d59f5c3bd8b3952ff894bc506933dcfa96
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-15 19:48:34 +0530

    Enhance DreamJob component by adding responsive utility classes for improved layout across various screen sizes, ensuring a better user experience on mobile and desktop devices.

--- src/components/landingPage/dreamJob.tsx
+++ src/components/landingPage/dreamJob.tsx
@@ -3,11 +3,11 @@ import {IconSearch} from "@tabler/icons-react";
 
 const DreamJob = () => {
     return (
-        <div className={'flex items-center px-16'}>
-            <div className={'flex flex-col w-[45%] gap-3'}>
-                <div className={'text-6xl font-bold leading-tight text-mine-shaft-100 [&>span]:text-bright-sun-400'}>Find your <span>dream job</span> with us</div>
-                <div className={'text-lg text-mine-shaft-200'}>Good life begins with a good company. Start exploring thousands of jobs in one place.</div>
-                <div className={'flex gap-3 mt-5'}>
+        <div className={'flex items-center px-16 bs-mx:px-10 md-mx:px-5 sm-mx:flex-col-reverse '}>
+            <div className={'flex flex-col w-[45%] gap-3 md-mx:w-full'}>
+                <div className={'text-6xl font-bold leading-tight text-mine-shaft-100 [&>span]:text-bright-sun-400 bs-mx:text-5xl md-mx:text-4xl sm-mx:text-3xl'}>Find your <span>dream job</span> with us</div>
+                <div className={'text-lg text-mine-shaft-200 md-mx:text-sm'}>Good life begins with a good company. Start exploring thousands of jobs in one place.</div>
+                <div className={'flex items-center gap-3 mt-5'}>
                     <TextInput className={'bg-mine-shaft-900 rounded-lg p-1 px-2 text-mine-shaft-100 [&_input]:!text-mine-shaft-100'} variant={'unstyled'} label={'Job Title'} placeholder={'Software Engineer'}/>
 
                     <TextInput className={'bg-mine-shaft-900 rounded-lg p-1 px-2 text-mine-shaft-100 [&_input]:!text-mine-shaft-100'} variant={'unstyled'} label={'Job Type'} placeholder={'Full time'}/>
@@ -17,10 +17,10 @@ const DreamJob = () => {
                     </div>
                 </div>
             </div>
-            <div className={'flex items-center justify-center w-[55%]'}>
+            <div className={'flex items-center justify-center w-[55%] sm-mx:w-full'}>
                 <div className={'w-[30rem] relative'}>
                     <img src={'/src/assets/Boy.png'} alt={'boy'}/>
-                    <div className={'absolute -right-10 top-[50%] w-fit border-bright-sun-400 border rounded-lg p-3 backdrop-blur-md'}>
+                    <div className={'absolute -right-10 top-[50%] w-fit border-bright-sun-400 border rounded-lg p-3 backdrop-blur-md bs-mx:right-0'}>
                         <div className="text-center mb-1 text-sm  text-mine-shaft-100">10K+ got jobs</div>
                         <AvatarGroup>
                             <Avatar src={'/src/assets/avatar.png'}/>
@@ -29,7 +29,7 @@ const DreamJob = () => {
                             <Avatar>+5</Avatar>
                         </AvatarGroup>
                     </div>
-                    <div className={'absolute -left-10 top-[25%] w-fit border-bright-sun-400 border rounded-lg p-3 backdrop-blur-md flex flex-col gap-3'}>
+                    <div className={'absolute -left-10 top-[25%] w-fit border-bright-sun-400 border rounded-lg p-3 backdrop-blur-md flex flex-col gap-3 bs-mx:top-[35%] xs-mx:top-[40%] sm-mx:left-1'}>
                         <div className={'flex items-center gap-2'}>
                             <div className={'w-10 h-10 p-1 bg-mine-shaft-900 rounded-lg'}>
                                 <img src={'/src/assets/icons/Google.png'} alt={'google'}/>
