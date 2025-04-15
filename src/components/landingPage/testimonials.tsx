c698f8f7fa3e072d5fd775284b11a8b3d68edc42
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-15 20:41:32 +0530

    Enhance Testimonials component by adding responsive utility classes for improved layout across various screen sizes, ensuring a better user experience on mobile and desktop devices.

--- src/components/landingPage/testimonials.tsx
+++ src/components/landingPage/testimonials.tsx
@@ -3,22 +3,22 @@ import {testimonials} from "@/Data/Data.tsx";
 
 const Testimonials = () => {
     return (
-        <div className={'mt-20 pb-5'}>
-            <div className={'text-4xl text-center font-semibold text-mine-shaft-100 mb-3'}>
+        <div className={'mt-20 p-5'}>
+            <div className={'text-4xl text-center font-semibold text-mine-shaft-100 mb-3 md-mx:text-3xl sm-mx:text-2xl xs-mx:text-xl'}>
                 What <span className={'text-bright-sun-400'}>
                 user
             </span> say's about us ?
             </div>
-            <div className={'flex justify-evenly'}>
+            <div className={'flex justify-evenly md-mx:flex-wrap'}>
 
             {
                 testimonials.map((data, index) => (
                     <div key={index}
-                         className={'flex flex-col gap-3 w-[23%] border rounded-xl mt-10 border-bright-sun-400 p-3'}>
+                         className={'flex flex-col gap-3 w-[23%] border rounded-xl mt-10 border-bright-sun-400 p-3 md-mx:w-[46%] xs-mx:w-full'}>
                         <div className={'flex gap-2 items-center'}>
                             <Avatar className={'!h-14 !w-14'} src="/src/assets/avatar.png" alt={"it's me"}/>
                             <div>
-                                <div className={'text-lg text-mine-shaft-100 font-semibold'}>{data.name}</div>
+                                <div className={'text-lg text-mine-shaft-100 font-semibold sm-mx:text-base xs-mx:text-sm'}>{data.name}</div>
                                 <Rating value={3.5} fractions={2} readOnly/>
                             </div>
                         </div>
