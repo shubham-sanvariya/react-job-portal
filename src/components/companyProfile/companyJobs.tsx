28d5402d4a32097f2b8be82095183ad4efffcd47
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-16 20:41:14 +0530

    Implement job fetching in CompanyJobs component using getJobsByCompanyName, replacing static jobList with dynamic data based on company name, enhancing user experience and data accuracy.

--- src/components/companyProfile/companyJobs.tsx
+++ src/components/companyProfile/companyJobs.tsx
@@ -1,12 +1,36 @@
-import {jobList} from "@/Data/JobsData.tsx";
 import JobCard from "@/components/findjobs/jobCard.tsx";
+import {useEffect, useRef, useState} from "react";
+import {getJobsByCompanyName} from "@/services/jobService.tsx";
+import {useParams} from "react-router-dom";
+import axios from "axios";
+import {JobType} from "@/types/jobType.ts";
+import {errorNotification} from "@/services/notificationUtils.tsx";
 
 const CompanyJobs = () => {
+    const { name } = useParams();
+    const ref = useRef(false);
+    const [companyJobs, setCompanyJobs] = useState<JobType[] | null>(null);
+
+    useEffect(() => {
+        if (ref.current || !name) return;
+
+        getJobsByCompanyName(name)
+            .then(res => setCompanyJobs(res.content))
+            .catch((err : unknown) => {
+                if (axios.isAxiosError(err)){
+                    errorNotification("Failed to fetch jobs of company",err.response?.data?.errorMessage);
+                }else {
+                    errorNotification("An unexpected error occurred","Something went wrong with the server");
+                }
+            })
+        ref.current = true;
+    }, [name]);
+
     return (
         <div className={'flex m-10' +
             ' flex-wrap gap-3'}>
             {
-                jobList.map((job,index) => <JobCard key={index} {...job}/>)
+                companyJobs?.map((job, index) => <JobCard key={index} {...job}/>)
             }
         </div>
     )
