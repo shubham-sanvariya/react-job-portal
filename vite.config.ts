b42aabbc8e71fa16972b5f993ba1d113ea1d9736
shubham-sanwariya <shubhamsanvariyait@gmail.com>
2025-04-16 23:40:50 +0530

    Add vite-plugin-static-copy to Vite configuration for static asset handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import {viteStaticCopy} from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteStaticCopy(({
+    targets: [
+      {
+        src: 'src/assets',
+        dest: ''
+      }
+    ]
+  }))],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
